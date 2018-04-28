c4f86e8c9622def170be25babab67e65cb184916
Joy <joywangx@sina.com>
2018-04-28 11:07:02 +0800

    update

--- xingstation_release/src/page/project/item/index.vue
+++ xingstation_release/src/page/project/item/index.vue
@@ -86,6 +86,8 @@
 </template>
 
 <script>
+import project from 'service/project'
+
 import { Button, Input, Table, TableColumn, Pagination, Form, FormItem, MessageBox, DatePicker} from 'element-ui'
 
 export default {
@@ -138,7 +140,18 @@ export default {
   },
   mounted() {
   },
+  created () {
+    this.getProjectList()
+  },
   methods: {
+    getProjectList () {
+       project.getProjectList(this).then(response => {
+         console.log(response)
+      }).catch(error => {
+        console.log(error)
+        // this.setting.loading = false;
+      })
+    },
     search (formName) {
       this.$refs[formName].validate((valid) => {
         if (valid) {
