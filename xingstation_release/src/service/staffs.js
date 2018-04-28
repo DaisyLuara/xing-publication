a4f8856e259ad132679849cad2066a3131a6051d
Joy <joywangx@sina.com>
2018-04-28 14:20:15 +0800

    Update

--- xingstation_release/src/service/staffs.js
+++ xingstation_release/src/service/staffs.js
@@ -3,7 +3,7 @@ const STAFFS_API = '/api/staffs'
 export default {
   getStaffsList(context,args) {
     return new Promise(function(resolve, reject){
-      context.$http.get(STAFFS_API, {params: args}).then(response => {
+      context.$http.get(STAFFS_API).then(response => {
         resolve(response.data)
       }).catch(error => {
         reject(error)
