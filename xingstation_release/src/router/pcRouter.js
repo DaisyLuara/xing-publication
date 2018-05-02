ca2fdce99ef06e75dd240608d72157559defbcce
Joy <joywangx@sina.com>
2018-05-02 10:59:22 +0800

    update

--- xingstation_release/src/router/pcRouter.js
+++ xingstation_release/src/router/pcRouter.js
@@ -1,12 +1,12 @@
 import pcHome from 'page/home'
 import system from 'router/pc/system'
-import main from 'router/pc/main'
+// import main from 'router/pc/main'
 import customer from 'router/pc/customer'
 import project from 'router/pc/project'
 import inform from 'router/pc/inform'
 import account from 'router/pc/account'
-import feedback from 'router/pc/feedback'
-import help from 'router/pc/help'
+// import feedback from 'router/pc/feedback'
+// import help from 'router/pc/help'
 import contract from 'router/pc/contract'
 
 export default {
@@ -17,14 +17,14 @@ export default {
     permission: 'main',
   },
   children: [
-    main,
+    // main,
     system,
     customer,
     project,
     contract,
     inform,
     account,
-    feedback,
-    help
+    // feedback,
+    // help
   ],
 }
