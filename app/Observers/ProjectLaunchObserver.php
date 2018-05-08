2d43f2f4b882778b67577256c5c4957294c2d555
caesar chan <caeasar_chan@live.com>
2018-05-08 22:12:14 +0800

    update

--- app/Observers/ProjectLaunchObserver.php
+++ app/Observers/ProjectLaunchObserver.php
@@ -7,25 +7,25 @@ use App\Models\ProjectLaunchLocal;
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
-class UserObserver
+class ProjectLaunchObserver
 {
-    public function saving(ProjectLaunchLocal $projectLaunchLocal)
-    {
-
-        if (empty($projectLaunchLocal->default_plid)) {
-            $projectLaunchLocal->default_plid = 0;
-        }
-
-        if (empty($projectLaunchLocal->weekday_tvid)) {
-            $projectLaunchLocal->weekday_tvid = 0;
-        }
-
-        if (empty($projectLaunchLocal->weekend_tvid)) {
-            $projectLaunchLocal->default_plid = 0;
-        }
-
-        $projectLaunchLocal->cid = 1007;
-        $projectLaunchLocal->pid = 4;
-
-    }
+//    public function saving(ProjectLaunchLocal $projectLaunchLocal)
+//    {
+//
+//        if (empty($projectLaunchLocal->default_plid)) {
+//            $projectLaunchLocal->default_plid = 0;
+//        }
+//
+//        if (empty($projectLaunchLocal->weekday_tvid)) {
+//            $projectLaunchLocal->weekday_tvid = 0;
+//        }
+//
+//        if (empty($projectLaunchLocal->weekend_tvid)) {
+//            $projectLaunchLocal->default_plid = 0;
+//        }
+//
+//        $projectLaunchLocal->cid = 1007;
+//        $projectLaunchLocal->pid = 4;
+//
+//    }
 }
