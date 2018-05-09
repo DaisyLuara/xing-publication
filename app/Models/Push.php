a5376d835d2023dbb34a44ad42b0b4d2d07c0eab
yangyn <Yq*94614>
2018-05-09 21:25:23 +0800

    update

--- app/Models/Push.php
+++ app/Models/Push.php
@@ -9,7 +9,11 @@ class Push extends Model
     protected $connection='ar';
     public $table='push';
 
-    function point(){
+    public function point(){
         return $this->belongsTo(Point::class,'oid','oid');
     }
+
+    public function project(){
+        return $this->belongsTo(Project::class,'alias','versionname');
+    } 
 }
--- app/Models/WxThird.php
+++ app/Models/WxThird.php
@@ -9,4 +9,8 @@ class WxThird extends Model
     protected $connection='ar';
     public $table='wx_third_info';
 
+    public function projectAdLaunch()
+    {
+        return $this->hasOne(ProjectAdLaunch::class,'wiid','id');
+    }
 }
