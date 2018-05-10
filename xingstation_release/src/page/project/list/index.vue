f6ef2758be4bb03c6d157d0c2063111f5c59c7b0
jun <Synac1012@aliyun.com>
2018-05-10 15:22:29 +0800

    update

---
+++ xingstation_release/src/page/project/list/index.vue
@@ -0,0 +1,203 @@
+<template>
+  <div class="root">
+    <div class="item-list-wrap" :element-loading-text="setting.loadingText" v-loading="setting.loading">
+      <div class="item-content-wrap">
+      <div class="total-wrap">
+          <span class="label">
+            总数:{{pagination.total}} 
+          </span>
+        </div>
+        <el-table :data="tableData" style="width: 100%" highlight-current-row >
+          <el-table-column type="selection" width="55" ></el-table-column>
+          <el-table-column
+            prop="id"
+            label="ID"
+            width="130"
+            fixed
+            >
+          </el-table-column>
+          <el-table-column
+            prop="img"
+            label="产品"
+            width="140"
+            >
+           <template slot-scope="scope">
+              <img :src="scope.row.img" alt="" class="icon-item"/>
+            </template>
+          </el-table-column>
+          <el-table-column
+            prop="area"
+            label="图标"
+            width="130"
+            >
+          </el-table-column>
+          <el-table-column
+            prop="market"
+            label="封面"
+            min-width="150"
+            >
+          </el-table-column>
+          <el-table-column
+            prop="point"
+            label="连接"
+            min-width="200">
+            <template slot-scope="scope">
+              <span>查看</span>
+            </template>
+          </el-table-column>
+          <el-table-column
+            prop="faceDate"
+            label="版本"
+            min-width="180">
+          </el-table-column>
+          <el-table-column
+            prop="networkDate"
+            label="版本号"
+            min-width="180"
+            >
+          </el-table-column>
+          <el-table-column
+            prop="screenStatus"
+            label="时间"
+            min-width="180">
+          </el-table-column>
+          <el-table-column label="操作" width="150" fixed="right">
+            <template slot-scope="scope">
+              <el-button size="small" type="primary" @click="linkToEdit()">推送</el-button> 
+              <el-button size="small" type="warning" @click="showData()">编辑</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        <div class="pagination-wrap">
+          <el-pagination
+          layout="prev, pager, next, jumper, total"
+          :total="pagination.total"
+          :page-size="pagination.pageSize"
+          :current-page="pagination.currentPage"
+          @current-change="changePage"
+          >
+          </el-pagination>
+        </div>
+      </div>  
+    </div>
+  </div>
+</template>
+
+<script>
+import equipment from 'service/equipment'
+import search from 'service/search'
+
+import { Button, Input, Table, TableColumn, Pagination,Dialog, Form, FormItem, MessageBox, DatePicker, Select, Option, CheckboxGroup, Checkbox} from 'element-ui'
+
+export default {
+  data () {
+      return {
+      filters: {
+        name: ''
+      },
+      setting: {
+        loading: false,
+        loadingText: "拼命加载中"
+      },
+      dataValue: '',
+      loading: true,
+      arUserName: '',
+      dataShowFlag: true,
+      pagination: {
+        total: 0,
+        pageSize: 10,
+        currentPage: 1
+      },
+      tableData: []
+    }
+  },
+  mounted() {
+  },
+  created () {
+    this.gettEquipmentList();
+    // let user_info = JSON.parse(localStorage.getItem('user_info'))
+    // this.arUserName = user_info.name
+    // this.dataShowFlag = user_info.roles.data[0].name === 'legal-affairs' ? false : true
+  },
+  methods: {
+   gettEquipmentList () {
+      this.setting.loadingText = "拼命加载中"
+      this.setting.loading = true;
+      let searchArgs = {
+        page : this.pagination.currentPage,
+        }
+      equipment.gettEquipmentList(this, searchArgs).then((response) => {
+       let data = response.data
+       this.tableData = data
+       console.log('=========');
+       console.log(response);
+       console.log('=========');
+       this.pagination.total = response.meta.pagination.total
+       this.setting.loading = false;
+      }).catch(error => {
+        console.log(error)
+      this.setting.loading = false;
+      })
+    },
+    
+    changePage (currentPage) {
+      alert(currentPage)
+      this.pagination.currentPage = currentPage
+      this.gettEquipmentList ();
+    },
+     
+
+  },
+  components: {
+    "el-table": Table,
+    "el-date-picker": DatePicker,
+    "el-table-column":  TableColumn,
+    "el-button": Button,
+    "el-input": Input,
+    "el-pagination": Pagination,
+    "el-form": Form,
+    "el-form-item": FormItem,
+    'el-select': Select,
+    'el-option': Option,
+    'el-checkbox-group': CheckboxGroup,
+    'el-checkbox': Checkbox,
+    'el-dialog':Dialog
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  .root {
+    font-size: 14px;
+    color: #5E6D82;
+    .item-list-wrap{
+      background: #fff;
+      padding: 30px;
+      .el-form-item{
+        margin-bottom: 0;
+      }
+      .item-content-wrap{
+        .icon-item{
+          padding: 10px;
+          width: 50%;
+        }
+        .total-wrap{
+          margin-top: 5px;
+          display: flex;
+          flex-direction: row;
+          justify-content: space-between;
+          font-size: 16px;
+          align-items: center;
+          margin-bottom: 10px;
+          .label {
+            font-size: 14px;
+          }
+        }
+        .pagination-wrap{
+          margin: 10px auto;
+          text-align: right;
+        }
+      }
+    }
+  }
+</style>
