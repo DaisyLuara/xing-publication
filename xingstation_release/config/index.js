14d5b4609a41e596480967668c654143c2f2a315
jun <Synac1012@aliyun.com>
2018-05-14 20:32:14 +0800

    update

--- xingstation_release/config/index.js
+++ xingstation_release/config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/tower': {
-        target: 'https://tower.im/',//设置你调用的接口域名和端口号 别忘了加http
+        target: 'http://tower.im/',//设置你调用的接口域名和端口号 别忘了加http
         changeOrigin: true,
         pathRewrite: {
         '^/tower': '/'//这里理解成用‘/api’代替target里面的地址，后面组件中我们掉接口时直接用api代替 比如我要调用'http://192.168.8.159:9999/'，直接写‘/api/user/add’即可
