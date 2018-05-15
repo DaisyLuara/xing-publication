c69fea12d6e82e16b61de7c885ced6eef55c2d3f
Joy <joywangx@sina.com>
2018-05-15 20:57:05 +0800

    Update

--- xingstation_release/config/prod.env.js
+++ xingstation_release/config/prod.env.js
@@ -2,6 +2,6 @@
 module.exports = {
     NODE_ENV: '"production"',
     SERVER_URL: '"http://papi.xingstation.com"',
-    HTTPS_SERVER_URL: '"https://papi.newgls.cn"',
+    HTTPS_SERVER_URL: '"https://papi.xingstation.cn"',
     TOWER_URL: '"http://tower.im/"'
 }
