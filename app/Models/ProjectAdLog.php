ed9be4abc839f710fd635cb93319278694720f39
yangyn <Yq*94614>
2018-05-17 16:55:35 +0800

    update

--- app/Models/ProjectAdLog.php
+++ app/Models/ProjectAdLog.php
@@ -1,28 +1,25 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: yangqiang
- * Date: 2018/5/17
- * Time: 14:09
- */
 
 namespace App\Models;
 
 
 class ProjectAdLog extends Model
 {
-    protected $connection='ar';
-    public $table='ar_product_ad_log';
+    protected $connection = 'ar';
+    public $table = 'ar_product_ad_log';
 
-    public function point(){
-        return $this->belongsTo(Point::class,'oid','oid');
+    public function point()
+    {
+        return $this->belongsTo(Point::class, 'oid', 'oid');
     }
 
-    public function wxThird(){
-        return $this->belongsTo(WxThird::class,'wiid','id');
+    public function wxThird()
+    {
+        return $this->belongsTo(WxThird::class, 'wiid', 'id');
     }
 
-    public function project(){
-        return $this->belongsTo(Project::class,'piid','id');
+    public function project()
+    {
+        return $this->belongsTo(Project::class, 'piid', 'id');
     }
 }
--- app/Scopes/FaceCountScope.php
+++ app/Scopes/FaceCountScope.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: yangqiang
- * Date: 2018/5/15
- * Time: 15:59
- */
 
 namespace App\Scopes;
 
