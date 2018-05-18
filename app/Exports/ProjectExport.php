7fc3a6cc8c2a3edc225d3361b6f0f147e15a21fe
yangyn <Yq*94614>
2018-05-18 11:45:24 +0800

    update

--- app/Exports/ProjectExport.php
+++ app/Exports/ProjectExport.php
@@ -15,47 +15,51 @@ class ProjectExport implements FromCollection, WithStrictNullComparison, WithEve
 {
     public function __construct($request)
     {
-        $this->date = $request->date;
+        $this->start_date = $request->start_date;
+        $this->end_date = $request->end_date;
     }
 
     public function collection()
     {
-        $date1 = date_format((new Carbon($this->date))->addMonth(-5), 'Y-m');
-        $date2 = date_format((new Carbon($this->date))->addMonth(-4), 'Y-m');
-        $date3 = date_format((new Carbon($this->date))->addMonth(-3), 'Y-m');
-        $date4 = date_format((new Carbon($this->date))->addMonth(-2), 'Y-m');
-        $date5 = date_format((new Carbon($this->date))->addMonth(-1), 'Y-m');
-        $date6 = date_format(new Carbon($this->date), 'Y-m');
+        $startDate = $this->start_date;
+        $endDate = $this->end_date;
+        $dates = [$startDate];
+        while ((new Carbon($endDate))->gt(new Carbon($startDate))) {
+            $startDate = date_format((new Carbon($startDate))->addMonth(1), 'Y-m');
+            $dates[] = $startDate;
+        }
 
+        $this->dateNum = sizeof($dates);
         $faceCount = DB::connection('ar')->table('face_count_log')
             ->join('ar_product_list', 'belong', '=', 'versionname')
             ->join('avr_official', 'face_count_log.oid', '=', 'avr_official.oid')
             ->join('avr_official_area', 'avr_official.areaid', '=', 'avr_official_area.areaid')
             ->join('avr_official_market', 'avr_official.marketid', '=', 'avr_official_market.marketid')
-            ->whereRaw("date_format(face_count_log.date, '%Y-%m') BETWEEN '$date1' AND '$date6' and avr_official.oid not in ('16', '19', '30', '31', '335', '334', '329', '328', '327')")
+            ->whereRaw("date_format(face_count_log.date, '%Y-%m') BETWEEN '$this->start_date' AND '$this->end_date' and avr_official.oid not in ('16', '19', '30', '31', '335', '334', '329', '328', '327')")
             ->groupby(DB::raw("belong,date_format(face_count_log.date,'%Y-%m')"))
             ->selectRaw("ar_product_list.name as projectName,date_format(face_count_log.date,'%Y-%m') as date, sum(looknum) as lookNum ,sum(playernum) as playerNum ,sum(lovenum) as loveNum,sum(outnum) as outNum,sum(scannum) as scanNum");
 
-        $sql1 = "a.projectName ,max(case a.date when '$date1' then concat_ws(',',cast(a.looknum as char),cast(a.playernum as char),cast(a.lovenum as char),cast(a.outnum as char),cast(a.scannum as char)) else 0 end) '$date1'";
-        $sql2 = ",max(case a.date when '$date2' then concat_ws(',',cast(a.lookNum as char),cast(a.playerNum as char),cast(a.loveNum as char),cast(a.outNum as char),cast(a.scanNum as char)) else 0 end) '$date2'";
-        $sql3 = ",max(case a.date when '$date3' then concat_ws(',',cast(a.lookNum as char),cast(a.playerNum as char),cast(a.loveNum as char),cast(a.outNum as char),cast(a.scanNum as char)) else 0 end) '$date3'";
-        $sql4 = ",max(case a.date when '$date4' then concat_ws(',',cast(a.lookNum as char),cast(a.playerNum as char),cast(a.loveNum as char),cast(a.outNum as char),cast(a.scanNum as char)) else 0 end) '$date4'";
-        $sql5 = ",max(case a.date when '$date5' then concat_ws(',',cast(a.lookNum as char),cast(a.playerNum as char),cast(a.loveNum as char),cast(a.outNum as char),cast(a.scanNum as char)) else 0 end) '$date5'";
-        $sql6 = ",max(case a.date when '$date6' then concat_ws(',',cast(a.lookNum as char),cast(a.playerNum as char),cast(a.loveNum as char),cast(a.outNum as char),cast(a.scanNum as char)) else 0 end) '$date6'";
-        $sql = $sql1 . $sql2 . $sql3 . $sql4 . $sql5 . $sql6;
+        $Max = "";
+        for ($i = 0; $i < sizeof($dates); $i++) {
+            $Max = $Max . ",max(case a.date when '$dates[$i]' then concat_ws(',',cast(a.lookNum as char),cast(a.playerNum as char),cast(a.loveNum as char),cast(a.outNum as char),cast(a.scanNum as char)) else 0 end) '$dates[$i]'";
+        }
+
         $faceCount = DB::connection('ar')->table(DB::raw("({$faceCount->toSql()}) as a"))
-            ->selectRaw("$sql")
+            ->selectRaw("a.projectName " . $Max)
             ->groupBy(DB::raw('a.projectName'))
             ->get();
 
         $data = collect();
-        $header1 = ['节目名称', $date1, '', '', '', '', $date2, '', '', '', '', $date3, '', '', '', '', $date4, '', '', '', '', $date5, '', '', '', '', $date6, '', '', '', ''];
+        $header1 = ['节目名称'];
+        for ($i = 0; $i < sizeof($dates); $i++) {
+            $header1 = array_merge($header1, [$dates[$i], '', '', '', '']);
+        }
         $header2 = [''];
-        for ($i = 0; $i < 6; $i++) {
+        for ($i = 0; $i < sizeof($dates); $i++) {
             $header2 = array_merge($header2, ['', '', '', '', '']);
         }
         $header3 = [''];
-        for ($i = 0; $i < 6; $i++) {
+        for ($i = 0; $i < sizeof($dates); $i++) {
             $header3 = array_merge($header3, ['围观数', '玩家数', '会员数', '生成数', '扫码数']);
         }
         $data->push($header1);
@@ -95,7 +99,7 @@ class ProjectExport implements FromCollection, WithStrictNullComparison, WithEve
         return [
             AfterSheet::class => function (AfterSheet $event) {
 
-                $event->sheet->getDelegate()->getStyle('A1:AE' . $this->data->count())->applyFromArray([
+                $event->sheet->getDelegate()->getStyle('A1:' . $this->change($this->dateNum * 5) . $this->data->count())->applyFromArray([
                     'borders' => [
                         'allBorders' => [
                             'borderStyle' => Border::BORDER_THIN,
@@ -103,23 +107,41 @@ class ProjectExport implements FromCollection, WithStrictNullComparison, WithEve
                     ]
                 ]);
 
-                $event->sheet->getDelegate()->setMergeCells(['A1:A3', 'B1:F2', 'G1:K2', 'L1:P2', 'Q1:U2', 'V1:Z2', 'AA1:AE2']);
+                $cellArray = ['A1:A3'];
+                for ($i = 0; $i < $this->dateNum; $i++) {
+                    $startNum = 1 + 5 * $i;
+                    $endNum = 5 * ($i + 1);
+                    $cellArray[] = $this->change($startNum) . '1:' . $this->change($endNum) . '2';
+                }
+
+                $event->sheet->getDelegate()->setMergeCells($cellArray);
                 //居中
                 $event->sheet->getDelegate()
-                    ->getStyle('A1:AE' . $this->data->count())
+                    ->getStyle('A1:' . $this->change($this->dateNum * 5) . $this->data->count())
                     ->getAlignment()
                     ->setHorizontal(Alignment::HORIZONTAL_CENTER)
                     ->setVertical(Alignment::VERTICAL_CENTER);
                 //表头加粗
                 $event->sheet->getDelegate()
-                    ->getStyle('A1:AE3')
+                    ->getStyle('A1:' . $this->change($this->dateNum * 5) . '3')
                     ->applyFromArray([
                         'font' => [
                             'bold' => 'true'
                         ]
                     ]);
-                $event->sheet->getDelegate()->freezePane('A4');
+                $event->sheet->getDelegate()->freezePane('B4');
             }
         ];
+    }
+
+    public function change($x)
+    {
+        $map = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+        $t = "";
+        while ($x >= 0) {
+            $t = $map[$x % 26] . $t;
+            $x = floor($x / 26) - 1;
+        }
+        return $t;
     }
 }
