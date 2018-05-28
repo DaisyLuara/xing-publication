1deb22e773a8749e65cbc169ccab697a691bddc3
caesar chan <caeasar_chan@live.com>
2018-05-28 20:38:47 +0800

    update

--- app/Http/Controllers/Admin/Project/V1/routes.php
+++ app/Http/Controllers/Admin/Project/V1/routes.php
@@ -1,6 +1,6 @@
 <?php
 $api->version('v1', [
-    'namespace' => 'App\Http\Controllers\Admin\Device\V1\Api',
+    'namespace' => 'App\Http\Controllers\Admin\Point\V1\Api',
     'middleware' => ['serializer:array', 'bindings'] //api返回数据切换. Fractal 组件默认提供  DataArraySerializer ArraySerializer
 ], function ($api) {
     $api->group([
@@ -8,9 +8,11 @@ $api->version('v1', [
         'limit' => config('api.rate_limits.access.limit'),
         'expires' => config('api.rate_limits.access.expires'),
     ], function ($api) {
+
+        $api->get('point_configs/{point_config}', 'PointConfigController@show');//点位配置
+
         $api->group(['middleware' => "api.auth", 'model' => 'App\Models\User'], function ($api) {
-            //设备
-            $api->get('push', 'PushController@index');
+            //热力地图
             $api->get('point/map', 'PointController@map');
         });
     });
