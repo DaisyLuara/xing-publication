84dd9c2f5338a1f43c044ec22587079290e81a9a
yangyn <Yq*94614>
2018-05-31 16:45:39 +0800

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,11 @@ namespace App\Console;
 
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use Illuminate\Console\Scheduling\Schedule;
+use Carbon\Carbon;
+use DB;
+use EasyWeChat;
+use EasyWeChat\Kernel\Messages\Text;
+use App\Models\User;
 
 class Kernel extends ConsoleKernel
 {
@@ -23,6 +28,18 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $data = $this->getTenUser();
+        /** @var EasyWeChat\OfficialAccount\Application $officialAccount */
+        $officialAccount = EasyWeChat::officialAccount();
+        $schedule->call(function () use ($officialAccount, $data) {
+            for ($i = 0; $i < count($data); $i++) {
+                $text = new Text('你负责的点位“' . $data[$i]['pointName'] . '”上周日均围观数处于倒数第' . ($i + 1) . '名，日均围观数：' . $data[$i]['looknum']);
+//                $user = User::where('ar_user_id', '=', $data[$i]['uid']);
+//                $openId = $user->openid;
+                $openId = 'oNN6q0sZDI_OSTV6rl0rPeHjPgH8';
+                $officialAccount->customer_service->message($text)->to($openId)->send();
+            }
+        })->cron('0 0 10 ? * FRI');
     }
 
     /**
@@ -35,5 +52,33 @@ class Kernel extends ConsoleKernel
         $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
+    }
+
+    protected function getTenUser()
+    {
+        $startDate = Carbon::now()->addDay(-12)->toDateString();
+        $endDate = Carbon::now()->addDay(-5)->toDateString();
+        $faceCount = DB::connection('ar')->table('face_count_log as fcl')
+            ->join('admin_per_oid as apo', 'fcl.oid', '=', 'apo.oid')
+            ->join('avr_official as ao', 'fcl.oid', '=', 'ao.oid')
+            ->join('avr_official_area as aoa', 'ao.areaid', '=', 'aoa.areaid')
+            ->join('avr_official_market as aom', 'ao.marketid', '=', 'aom.marketid')
+            ->whereRaw(" date_format(fcl.date,'%Y-%m-%d') between '$startDate' and '$endDate' ")
+            ->where('belong', '<>', 'all')
+            ->whereNotIn('fcl.oid', [16, 19, 30, 31, 335, 334, 329, 328, 327])
+            ->groupBy('fcl.oid')
+            ->orderBy('looknum')
+            ->limit(10)
+            ->selectRaw("  apo.uid as uid,aoa.name as areaName,aom.name as marketName,ao.name as pointName,sum(looknum) as looknum")
+            ->get();
+        $data = [];
+        $faceCount->each(function ($item) use (&$data) {
+            $data[] = [
+                'uid' => $item->uid,
+                'pointName' => $item->areaName . '-' . $item->marketName . '-' . $item->pointName,
+                'looknum' => round($item->looknum / 7, 0)
+            ];
+        });
+        return $data;
     }
 }
--- app/Http/Controllers/Admin/Face/V1/Api/FaceCountController.php
+++ app/Http/Controllers/Admin/Face/V1/Api/FaceCountController.php
@@ -5,9 +5,25 @@ namespace App\Http\Controllers\Admin\Face\V1\Api;
 use App\Http\Controllers\Controller;
 use Carbon\Carbon;
 use DB;
+use EasyWeChat;
+use EasyWeChat\Kernel\Messages\Text;
 
 class FaceCountController extends Controller
 {
+    public function test()
+    {
+        $data = $this->getTenUser();
+        /** @var EasyWeChat\OfficialAccount\Application $officialAccount */
+        $officialAccount = EasyWeChat::officialAccount();
+        for ($i = 0; $i < count($data); $i++) {
+            $text = new Text('你负责的点位“' . $data[$i]['pointName'] . '”上周日均围观数处于倒数第' . ($i + 1) . '名，日均围观数：' . $data[$i]['looknum']);
+//                $user = User::where('ar_user_id', '=', $data[$i]['uid']);
+//                $openId = $user->openid;
+            $openId = 'oNN6q0sZDI_OSTV6rl0rPeHjPgH8';
+            $officialAccount->customer_service->message($text)->to($openId)->send();
+        }
+    }
+
     public function getTenUser()
     {
         $startDate = Carbon::now()->addDay(-12)->toDateString();
@@ -17,7 +33,7 @@ class FaceCountController extends Controller
             ->join('avr_official as ao', 'fcl.oid', '=', 'ao.oid')
             ->join('avr_official_area as aoa', 'ao.areaid', '=', 'aoa.areaid')
             ->join('avr_official_market as aom', 'ao.marketid', '=', 'aom.marketid')
-            ->whereRaw(" date_format(fcl.date,'%Y-%m-%d') between '$startDate' and '$endDate' ")
+            ->whereRaw(" date_format(fcl.date,'%Y-%m-%d') between '2018-04-01' and '2018-04-07' ")
             ->where('belong', '<>', 'all')
             ->whereNotIn('fcl.oid', [16, 19, 30, 31, 335, 334, 329, 328, 327])
             ->groupBy('fcl.oid')
