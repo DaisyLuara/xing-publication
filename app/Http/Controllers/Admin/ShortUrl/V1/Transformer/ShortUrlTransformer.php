817810e92bd5a2d01e0cd27679f1e05e7a48651b
caesar chan <caeasar_chan@live.com>
2018-06-01 10:01:56 +0800

    short url

--- app/Http/Controllers/Admin/ShortUrl/V1/Transformer/ShortUrlTransformer.php
+++ app/Http/Controllers/Admin/ShortUrl/V1/Transformer/ShortUrlTransformer.php
@@ -1,18 +1,15 @@
 <?php
 
-namespace App\Http\Controllers\Admin\Device\V1\Transformer;
+namespace App\Http\Controllers\Admin\ShortUrl\V1\Transformer;
 
-use App\Http\Controllers\Admin\Ad\V1\Transformer\ProjectTransformer;
-use App\Http\Controllers\Admin\Point\V1\Transformer\PointTransformer;
-use App\Http\Controllers\Admin\Device\V1\Models\ShortUrl;
+use App\Http\Controllers\Admin\ShortUrl\V1\Models\ShortUrl;
 use League\Fractal\TransformerAbstract;
-use Carbon\Carbon;
 
 class PushTransformer extends TransformerAbstract
 {
     protected $availableIncludes = ['point', 'project'];
 
-    public function transform(ShortUrl $push)
+    public function transform(ShortUrl $shortUrl)
     {
         return [
         ];
