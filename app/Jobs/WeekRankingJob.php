f7b66a542fe4d90b1d573d8f079e13400ac4b813
yangyn <Yq*94614>
2018-06-29 16:08:20 +0800

    update

--- app/Jobs/WeekRankingJob.php
+++ app/Jobs/WeekRankingJob.php
@@ -43,10 +43,10 @@ class WeekRankingJob implements ShouldQueue
             'touser' => "oNN6q0sZDI_OSTV6rl0rPeHjPgH8",
             'template_id' => 'siyJMjigeMMNpXrFSsvz6rvrKQh9Gf5RcfbiVYFQFyY',
             'data' => [
-                'first' => '你好，你的上周点位排名情况如下',
-                'keyword1' => $data['point_name'],
-                'keyword2' => "日均围观数：" . $data['looknum_average'] . "\r\n" . "点位排名：倒数第" . $data['ranking'] . "\r\n" . "场景分类：" . $data['scene_name'] . "\r\n" . "时间区间：" . $data['start_date'] . "至" . $data['end_date'],
-                'remark' => '再接再厉',
+                'first' => '你好，你的上周点位排名情况如下。',
+                'keyword1' => "点位名称 [" . $data['point_name'] . "]",
+                'keyword2' => "日均围观数 [" . $data['looknum_average'] . "]" . "\r\n" . "    点位排名 [倒数第" . $data['ranking'] . "]" . "\r\n" . "    场景分类 [" . $data['scene_name'] . "]" . "\r\n" . "    时间区间 [" . $data['start_date'] . " 至 " . $data['end_date'] . "]",
+                'remark' => '再接再厉！',
             ]
         ];
         $officialAccount->template_message->send($message);
