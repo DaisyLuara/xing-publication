7a6a8ad0445bfab20a7f9cc76df1c859ea97bdc3
yangyn <Yq*94614>
2018-07-06 14:50:43 +0800

    update

--- app/Exports/MarketingTopExport.php
+++ app/Exports/MarketingTopExport.php
@@ -27,13 +27,18 @@ class MarketingTopExport extends AbstractExport
     {
         $faceCount1 = DB::connection('ar')->table('face_count_log as fcl')
             ->join('ar_product_list as apl', 'belong', '=', 'versionname')
-            ->leftjoin('avr_official as ao', 'fcl.oid', '=', 'ao.oid')
-            ->leftjoin('avr_official_market as aom', 'ao.marketid', '=', 'aom.marketid')
+            ->join('avr_official as ao', 'fcl.oid', '=', 'ao.oid')
+            ->join('avr_official_market as aom', 'ao.marketid', '=', 'aom.marketid')
+            ->join('face_people_time_active_player', function ($join) {
+                $join->on('fcl.oid', '=', 'face_people_time_active_player.oid')
+                    ->on('fcl.belong', '=', 'face_people_time_active_player.belong')
+                    ->whereRaw("date_format(fcl.date,'%Y-%m-%d')=date_format(face_people_time_active_player.date,'%Y-%m-%d')");
+            }, null, null, 'left')
             ->whereRaw("date_format(fcl.date, '%Y-%m-%d') BETWEEN '{$this->startDate}' AND '{$this->endDate}' and fcl.oid not in ('16', '19', '30', '31', '335', '334', '329', '328', '327') and aom.marketid <> '15'")
-            ->groupBy(DB::raw("fcl.oid,belong"))
+            ->groupBy(DB::raw("fcl.oid,fcl.belong"))
             ->orderBy('apl.id')
             ->orderBy('looknum', 'desc')
-            ->selectRaw("apl.name as name,count(*) as days,sum(looknum) as lookNum ,sum(playernum) as playerNum ,sum(lovenum) as loveNum,sum(outnum) as outNum,sum(scannum) as scanNum");
+            ->selectRaw("apl.name as name,count(*) as days,sum(looknum) as lookNum ,sum(playernum7)as playerNum7,sum(playernum20) as playerNum20 ,sum(lovenum) as loveNum,sum(outnum) as outNum,sum(scannum) as scanNum");
 
         $faceCount2 = DB::connection('ar')->table(DB::raw("({$faceCount1->toSql()}) a,(select @gn := 0)  b"))
             ->selectRaw("  @gn := case when @name = name then @gn + 1 else 1 end gn,@name := name name,days,looknum,playernum,lovenum,outnum,scannum");
@@ -44,9 +49,9 @@ class MarketingTopExport extends AbstractExport
             ->groupBy('name')
             ->get();
         $data = collect();
-        $header1 = ['节目名称', '围观数', '', '活跃用户', '玩家数', '', '会员数', '', '生成数', '扫码数', '扫码率', '1', '2', '5', '7', '10', '20', '合计'];
-        $header2 = ['', '总数', '平均数', '总数', '总数', '平均数', '总数', '平均数', '', '', '', '刷脸', '活跃用户', '大玩家', '生成数', '扫码', '会员', ''];
-        $header3 = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''];
+        $header1 = ['节目名称', 'CPF', '', 'oCPF', '', 'CPR', '', '生成数', 'CPA', '扫码率', 'CPL', '', '1', '2', '5', '4', '10', '20', '合计'];
+        $header2 = ['', '总数', '平均数', '总数', '平均数', '总数', '平均数', '', '', '', '总数', '平均数', 'CPF', 'oCPF', 'CPR', '生成数', 'CPA', 'CPL', ''];
+        $header3 = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''];
         $data->push($header1);
         $data->push($header2);
         $data->push($header3);
@@ -56,28 +61,28 @@ class MarketingTopExport extends AbstractExport
                 'name' => $item['name'],
                 'lookNum' => $item['lookNum'],
                 'look_average' => round($item['lookNum'] / $item['pushNum'], 0),
-                'activeNum' => ($item['playerNum'] * 2 > $item['lookNum']) ? $item['lookNum'] : $item['playerNum'] * 2,
-                'playerNum' => $item['playerNum'],
-                'player_average' => round(($item['playerNum'] / $item['pushNum']), 0),
-                'loveNum' => $item['loveNum'],
-                'love_average' => round(($item['loveNum'] / $item['pushNum']), 0),
+                'player7' => $item['playerNum7'] ? $item['playerNum7'] : 0,
+                'player7_average' => round(($item['playerNum7'] / $item['pushNum']), 0),
+                'player20' => $item['playerNum20'] ? $item['playerNum20'] : 0,
+                'player20_average' => round(($item['playerNum20'] / $item['pushNum']), 0),
                 'outNum' => $item['outNum'],
                 'scanNum' => $item['scanNum'],
                 'rate' => (round(($item['outNum'] == 0) ? 0 : $item['scanNum'] / $item['outNum'], 2) * 100) . '%',
-
+                'loveNum' => $item['loveNum'],
+                'love_average' => round(($item['loveNum'] / $item['pushNum']), 0),
             ];
             $faceMoney = round($item['lookNum'] * 0.01, 0);
-            $activeMoney = round($item['activeNum'] * 0.02, 0);
-            $playerMoney = round($item['playerNum'] * 0.05, 0);
-            $outMoney = round($item['outNum'] * 0.07, 0);
+            $player7Money = round($item['player7'] * 0.02, 0);
+            $player20Money = round($item['player20'] * 0.05, 0);
+            $outMoney = round($item['outNum'] * 0.04, 0);
             $scanMoney = round($item['scanNum'] * 0.1, 0);
             $loveMoney = round($item['loveNum'] * 0.2, 0);
-            $totalMoney = $faceMoney + $activeMoney + $playerMoney + $outMoney + $scanMoney + $loveMoney;
+            $totalMoney = $faceMoney + $player7Money + $player20Money + $outMoney + $scanMoney + $loveMoney;
 
 
             $item['face_money'] = $faceMoney;
-            $item['active_money'] = $activeMoney;
-            $item['player_money'] = $playerMoney;
+            $item['playerNum7_money'] = $player7Money;
+            $item['playerNum20_money'] = $player20Money;
             $item['out_money'] = $outMoney;
             $item['scan_money'] = $scanMoney;
             $item['love_money'] = $loveMoney;
@@ -94,17 +99,23 @@ class MarketingTopExport extends AbstractExport
     {
         return [
             AfterSheet::class => function (AfterSheet $event) {
-                $cellArray = ['A1:A3', 'B1:C1', 'B2:B3', 'C2:C3', 'D2:D3', 'E1:F1', 'E2:E3', 'F2:F3', 'G1:H1', 'G2:G3', 'H2:H3', 'I1:I3', 'J1:J3', 'K1:K3', 'L2:L3', 'M2:M3', 'N2:N3', 'O2:O3', 'P2:P3', 'Q2:Q3', 'R1:R3'];
+                $cellArray = [
+                    'A1:A3', 'B1:C1', 'D1:E1', 'F1:G1', 'K1:L1',
+                    'B2:B3', 'C2:C3', 'D2:D3', 'E2:E3', 'F2:F3',
+                    'G2:G3', 'H1:H3', 'I1:I3', 'J1:J3', 'K2:K3',
+                    'L2:L3', 'M2:M3', 'N2:N3', 'O2:O3', 'P2:P3',
+                    'Q2:Q3', 'R2:R3', 'S1:S3'
+                ];
                 $event->sheet->getDelegate()->setMergeCells($cellArray);
 
                 $event->sheet->getDelegate()
-                    ->getStyle('A1:R' . $this->data->count())
+                    ->getStyle('A1:S' . $this->data->count())
                     ->getAlignment()
                     ->setVertical(Alignment::VERTICAL_CENTER)
                     ->setHorizontal(Alignment::HORIZONTAL_CENTER);
 
                 $event->sheet->getDelegate()
-                    ->getStyle('A1:R2')
+                    ->getStyle('A1:S3')
                     ->applyFromArray([
                         'font' => [
                             'bold' => 'true'
@@ -112,7 +123,7 @@ class MarketingTopExport extends AbstractExport
                     ]);
 
                 $event->sheet->getDelegate()
-                    ->getStyle('A1:R' . $this->data->count())
+                    ->getStyle('A1:S' . $this->data->count())
                     ->applyFromArray([
                         'borders' => [
                             'allBorders' => [
