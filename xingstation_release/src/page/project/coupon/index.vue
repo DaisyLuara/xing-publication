a0e4430ac1ea527e10fe7b01a7dda1563676eeef
Joy <joywangx@sina.com>
2018-07-10 15:32:48 +0800

    coupon

--- xingstation_release/src/page/project/coupon/index.vue
+++ xingstation_release/src/page/project/coupon/index.vue
@@ -30,19 +30,19 @@
             <template slot-scope="scope">
               <el-form label-position="left" inline class="demo-table-expand">
                 <el-form-item label="公司">
-                  <span>{{scope.row.company_id}}</span> 
+                  <span>{{scope.row.company.name}}</span> 
                 </el-form-item>
                 <el-form-item label="创建人">
-                  <span>{{scope.row.user_name}}</span> 
+                  <span>{{scope.row.user.name}}</span> 
                 </el-form-item>
                 <el-form-item label="优惠券名称">
-                  <span>{{scope.row.coupon_name}}</span> 
+                  <span>{{scope.row.name}}</span> 
                 </el-form-item>
                 <el-form-item label="优惠券描述">
-                  <span>{{scope.row.coupon_desc}}</span> 
+                  <span>{{scope.row.description}}</span> 
                 </el-form-item>
                 <el-form-item label="图片">
-                  <a :href="scope.row.image" target="_blank" style="color: blue">查看</a> 
+                  <a :href="scope.row.image_url" target="_blank" style="color: blue">查看</a> 
                 </el-form-item>
                 <el-form-item label="金额">
                   <span>{{scope.row.amount}}</span> 
@@ -57,16 +57,16 @@
                   <span>{{scope.row.people_max_get}}</span> 
                 </el-form-item>
                 <el-form-item label="是否开启没人无限领取">
-                  <span>{{scope.row.pmg_status=== 1 ? '开启' : '关闭'}}</span> 
+                  <span>{{scope.row.pmg_status === '1' ? '开启' : '关闭'}}</span> 
                 </el-form-item>
                 <el-form-item label="每天最大获取数">
                   <span>{{scope.row.day_max_get}}</span> 
                 </el-form-item>
                 <el-form-item label="是否开启每天无限领取">
-                  <span>{{scope.row.dmg_status===1 ? '固定' : '不固定'}}</span> 
+                  <span>{{scope.row.dmg_status ==='1' ? '固定' : '不固定'}}</span> 
                 </el-form-item>
                 <el-form-item label="是否固定日期">
-                  <span>{{scope.row.is_fixed_date === 1 ? '固定' : '不固定'}}</span> 
+                  <span>{{scope.row.is_fixed_date === '1' ? '固定' : '不固定'}}</span> 
                 </el-form-item>
                 <el-form-item label="延后生效天数">
                   <span>{{scope.row.delay_effective_day}}</span> 
@@ -81,7 +81,7 @@
                   <span>{{ scope.row.end_date }}</span>
                 </el-form-item>
                 <el-form-item label="状态">
-                  <span>{{ scope.row.is_active === 1 ? '启用' :'停用' }}</span>
+                  <span>{{ scope.row.is_active === '1' ? '启用' :'停用' }}</span>
                 </el-form-item>
               </el-form>
             </template>
@@ -92,6 +92,9 @@
             min-width="100"
             :show-overflow-tooltip="true"
             >
+            <template slot-scope="scope">
+              {{scope.row.company.name}}
+            </template>
           </el-table-column>
           <el-table-column
             prop="user_name"
@@ -99,17 +102,13 @@
             min-width="100"
             :show-overflow-tooltip="true"
             >
+            <template slot-scope="scope">
+              {{scope.row.user.name}}
+            </template>
           </el-table-column>
           <el-table-column
-            prop="coupon_name"
+            prop="name"
             label="名称"
-            min-width="100"
-            :show-overflow-tooltip="true"
-            >
-          </el-table-column>
-          <el-table-column
-            prop="coupon_desc"
-            label="描述"
             min-width="100"
             :show-overflow-tooltip="true"
             >
@@ -146,18 +145,20 @@
             prop="start_date"
             label="开始时间"
             min-width="100"
-            :show-overflow-tooltip="true">
+            :show-overflow-tooltip="true"
+            >
           </el-table-column>
           <el-table-column
             prop="end_date"
             label="结束时间"
             min-width="100"
-            :show-overflow-tooltip="true">
+            :show-overflow-tooltip="true"
+            >
           </el-table-column>
           <el-table-column label="操作" min-width="150">
             <template slot-scope="scope">
               <el-button size="small" type="warning" @click='linkToEdit(scope.row)'>编辑</el-button>
-              <el-button size="small" type="danger" @click="deleteCoupon(scope.row)">删除</el-button>
+              <!-- <el-button size="small" type="danger" @click="deleteCoupon(scope.row)">删除</el-button> -->
             </template>
           </el-table-column>
         </el-table>
@@ -178,7 +179,7 @@
 
 <script>
 import coupon from 'service/coupon'
-import search from 'service/search'
+// import search from 'service/search'
 
 import {
   Button,
@@ -209,52 +210,11 @@ export default {
         pageSize: 10,
         currentPage: 1
       },
-      tableData: [
-        {
-          company_id: 1,
-          user_name: '王小虎',
-          coupon_name: '龙虾刑警',
-          coupon_desc:'',
-          image: 'http://image.exe666.com/1007/image/1530696265.png',
-          amount: 100,
-          count: 100,
-          stock: 90,
-          people_max_get: 1,
-          pmg_status: 1,
-          day_max_get: 3,
-          dmg_status: 0,
-          is_fixed_date: 1,
-          delay_effective_day: 3,
-          effective_day: 4,
-          start_date: '2018-03-09',
-          end_date: '2018-04-09',
-          is_active: 0
-        },
-        {
-          company_id: 1,
-          user_name: '王小虎',
-          coupon_name: '龙虾刑警',
-          coupon_desc:'',
-          image: 'http://image.exe666.com/1007/image/1530696265.png',
-          amount: 100,
-          count: 100,
-          stock: 90,
-          people_max_get: 1,
-          pmg_status: 0,
-          day_max_get: 3,
-          dmg_status: 1,
-          is_fixed_date: 0,
-          delay_effective_day: 3,
-          effective_day: 4,
-          start_date: '2018-03-09',
-          end_date: '2018-04-09',
-          is_active: 1
-        }
-      ]
+      tableData: []
     }
   },
   created() {
-    // this.getCouponList()
+    this.getCouponList()
   },
   methods: {
     linkToEdit(currentCoupon) {
@@ -265,6 +225,7 @@ export default {
     getCouponList() {
       this.setting.loading = true
       let args = {
+        include: 'user,company',
         page: this.pagination.currentPage,
         company_id: this.filters.company_id
       }
@@ -272,6 +233,7 @@ export default {
         .getCouponList(this, args)
         .then(response => {
           let data = response.data
+          console.log(data)
           this.tableData = data
           this.pagination.total = response.meta.pagination.total
           this.setting.loading = false
@@ -291,21 +253,21 @@ export default {
         .then(() => {
           this.setting.loadingText = '删除中'
           this.setting.loading = true
-          coupon
-            .deleteCoupon(this, id)
-            .then(response => {
-              this.setting.loading = false
-              this.$message({
-                type: 'success',
-                message: '删除成功！'
-              })
-              this.pagination.currentPage = 1
-              this.getCouponList()
-            })
-            .catch(error => {
-              this.setting.loading = false
-              console.log(error)
-            })
+          // coupon
+          //   .deleteCoupon(this, id)
+          //   .then(response => {
+          //     this.setting.loading = false
+          //     this.$message({
+          //       type: 'success',
+          //       message: '删除成功！'
+          //     })
+          //     this.pagination.currentPage = 1
+          //     this.getCouponList()
+          //   })
+          //   .catch(error => {
+          //     this.setting.loading = false
+          //     console.log(error)
+          //   })
         })
         .catch(e => {
           console.log(e)
