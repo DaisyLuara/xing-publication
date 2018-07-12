05a16858301cea9c0843a3b35ff3fbd5f38227c4
Joy <joywangx@sina.com>
2018-07-12 11:30:35 +0800

    配置

---
+++ xingstation_release/src/page/setting/strategy/routerView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="strategy-root">
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+export default {
+  name: 'strategyView',
+  data() {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  .schedule-root {
+    height: 100%;
+    margin: 10px;
+    background-color: #EFF2F7;
+  }
+</style>
