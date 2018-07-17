71134704f3bbdfabcfc1a49142d554f497a8bf8e
yangyn <Yq*94614>
2018-07-17 17:37:39 +0800

    update migrate

--- database/migrations/ar/2018_07_10_092820_create_xs_face_character_count_table.php
+++ database/migrations/ar/2018_07_10_092820_create_xs_face_character_count_table.php
@@ -13,27 +13,29 @@ class CreateXsFaceCharacterCountTable extends Migration
      */
     public function up()
     {
-        Schema::connection('ar')->create('xs_face_character_count', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('oid');
-            $table->string('belong', 20);
-            $table->string('time', 20);
-            $table->integer('century00_bnum');
-            $table->integer('century00_gnum');
-            $table->integer('century90_bnum');
-            $table->integer('century90_gnum');
-            $table->integer('century80_bnum');
-            $table->integer('century80_gnum');
-            $table->integer('century70_bnum');
-            $table->integer('century70_gnum');
-            $table->integer('century0_bnum');
-            $table->integer('century0_gnum');
-            $table->timestamp('date')->nullable();
-            $table->bigInteger('clientdate');
-            $table->index('oid');
-            $table->index('belong');
-            $table->index('clientdate');
-        });
+        if (!Schema::connection('ar')->hasTable('xs_face_character_count')) {
+            Schema::connection('ar')->create('xs_face_character_count', function (Blueprint $table) {
+                $table->increments('id');
+                $table->integer('oid');
+                $table->string('belong', 20);
+                $table->string('time', 20);
+                $table->integer('century00_bnum');
+                $table->integer('century00_gnum');
+                $table->integer('century90_bnum');
+                $table->integer('century90_gnum');
+                $table->integer('century80_bnum');
+                $table->integer('century80_gnum');
+                $table->integer('century70_bnum');
+                $table->integer('century70_gnum');
+                $table->integer('century0_bnum');
+                $table->integer('century0_gnum');
+                $table->timestamp('date')->nullable();
+                $table->bigInteger('clientdate');
+                $table->index('oid');
+                $table->index('belong');
+                $table->index('clientdate');
+            });
+        }
     }
 
     /**
