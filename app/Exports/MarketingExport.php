6b1f0e12de7367f9f5af61d32b053bd3dfcd2b62
yangyn <Yq*94614>
2018-07-18 16:44:38 +0800

    update

--- app/Exports/MarketingExport.php
+++ app/Exports/MarketingExport.php
@@ -27,12 +27,12 @@ class MarketingExport extends AbstractExport
             ->whereNotIn('xs_face_count_log.oid', [16, 19, 30, 31, 177, 182, 327, 328, 329, 334, 335])
             ->groupby('xs_face_count_log.belong')
             ->orderBy('ar_product_list.name')
-            ->selectRaw('ar_product_list.name as name,count(xs_face_count_log.oid) as pushNum ,sum(looknum) as lookNum ,sum(playernum7) as playerNum7,sum(playernum20) as playerNum20 ,sum(lovenum) as loveNum,sum(outnum) as outNum,sum(scannum) as scanNum')
+            ->selectRaw('ar_product_list.name as name,count(xs_face_count_log.oid) as pushNum ,sum(looknum) as lookNum ,sum(playernum7) as playerNum7,sum(playernum20) as playerNum20 ,sum(playernum) as playerNum,sum(outnum) as outNum,sum(scannum) as scanNum,sum(lovenum) as loveNum')
             ->get();
         $data = collect();
-        $header1 = ['节目名称', 'CPF', '', 'oCPF', '', 'CPR', '', '生成数', 'CPA', '扫码率', 'CPL', '', '1', '2', '5', '4', '10', '20', '合计'];
-        $header2 = ['', '总数', '平均数', '总数', '平均数', '总数', '平均数', '', '', '', '总数', '平均数', 'CPF', 'oCPF', 'CPR', '生成数', 'CPA', 'CPL', ''];
-        $header3 = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''];
+        $header1 = ['节目名称', 'CPF', '', 'oCPF', '', 'CPR', '', '铁杆玩家', '', '生成数', 'CPA', '扫码率', 'CPL', '', '1', '2', '5', '4', '10', '20', '合计'];
+        $header2 = ['', '总数', '平均数', '总数', '平均数', '总数', '平均数', '总数', '平均数', '', '', '', '总数', '平均数', 'CPF', 'oCPF', 'CPR', '生成数', 'CPA', 'CPL', ''];
+        $header3 = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''];
         $data->push($header1);
         $data->push($header2);
         $data->push($header3);
@@ -46,6 +46,8 @@ class MarketingExport extends AbstractExport
                 'player7_average' => round(($item['playerNum7'] / $item['pushNum']), 0),
                 'player20' => $item['playerNum20'] ? $item['playerNum20'] : 0,
                 'player20_average' => round(($item['playerNum20'] / $item['pushNum']), 0),
+                'playerNum' => $item['playerNum'],
+                'player_average' => round(($item['playerNum'] / $item['pushNum']), 0),
                 'outNum' => $item['outNum'],
                 'scanNum' => $item['scanNum'],
                 'rate' => (round(($item['outNum'] == 0) ? 0 : $item['scanNum'] / $item['outNum'], 2) * 100) . '%',
@@ -80,22 +82,22 @@ class MarketingExport extends AbstractExport
         return [
             AfterSheet::class => function (AfterSheet $event) {
                 $cellArray = [
-                    'A1:A3', 'B1:C1', 'D1:E1', 'F1:G1', 'K1:L1',
+                    'A1:A3', 'B1:C1', 'D1:E1', 'F1:G1', 'H1:I1', 'M1:N1',
                     'B2:B3', 'C2:C3', 'D2:D3', 'E2:E3', 'F2:F3',
-                    'G2:G3', 'H1:H3', 'I1:I3', 'J1:J3', 'K2:K3',
-                    'L2:L3', 'M2:M3', 'N2:N3', 'O2:O3', 'P2:P3',
-                    'Q2:Q3', 'R2:R3', 'S1:S3'
+                    'G2:G3', 'H2:H3', 'I2:I3', 'J1:J3', 'K1:K3',
+                    'L1:L3', 'M2:M3', 'N2:N3', 'O2:O3', 'P2:P3',
+                    'Q2:Q3', 'R2:R3', 'S2:S3', 'T2:T3', 'U1:U3'
                 ];
                 $event->sheet->getDelegate()->setMergeCells($cellArray);
 
                 $event->sheet->getDelegate()
-                    ->getStyle('A1:S' . $this->data->count())
+                    ->getStyle('A1:U' . $this->data->count())
                     ->getAlignment()
                     ->setVertical(Alignment::VERTICAL_CENTER)
                     ->setHorizontal(Alignment::HORIZONTAL_CENTER);
 
                 $event->sheet->getDelegate()
-                    ->getStyle('A1:S3')
+                    ->getStyle('A1:U3')
                     ->applyFromArray([
                         'font' => [
                             'bold' => 'true'
@@ -103,7 +105,7 @@ class MarketingExport extends AbstractExport
                     ]);
 
                 $event->sheet->getDelegate()
-                    ->getStyle('A1:S' . $this->data->count())
+                    ->getStyle('A1:U' . $this->data->count())
                     ->applyFromArray([
                         'borders' => [
                             'allBorders' => [
