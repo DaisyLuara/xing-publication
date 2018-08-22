b475e02e8982fd36907d5e9d3912e71c55d2615f
yangyn <Yq*94614>
2018-08-22 17:02:33 +0800

    update

---
+++ app/Http/Controllers/Admin/WordFilter/V1/Models/WordFilter.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\Admin\WordFilter\V1\Models;
+
+use App\Models\Model;
+
+class WordFilter extends Model
+{
+    //
+}
--- app/Http/Controllers/Admin/WordFilter/V1/routes.php
+++ app/Http/Controllers/Admin/WordFilter/V1/routes.php
@@ -1,6 +1,6 @@
 <?php
 $api->version('v1', [
-    'namespace' => 'App\Http\Controllers\Admin\Face\V1\Api',
+    'namespace' => 'App\Http\Controllers\Admin\WordFilter\V1\Api',//修改test
     'middleware' => ['serializer:array', 'bindings'] //api返回数据切换. Fractal 组件默认提供  DataArraySerializer ArraySerializer
 ], function ($api) {
     $api->group([
@@ -9,8 +9,9 @@ $api->version('v1', [
         'expires' => config('api.rate_limits.access.expires'),
     ], function ($api) {
         $api->group(['middleware' => "api.auth", 'model' => 'App\Models\User'], function ($api) {
-            $api->get('week_ranking', 'FaceCountController@weekRanking');
+
         });
+        $api->get("word_filter", 'WordFilterController@filter');
     });
 
 });
