e2e789f34d09d08dfbc388ff39b3852ecc9e9338
caesar chan <caeasar_chan@live.com>
2018-09-11 10:42:06 +0800

    场地过滤

--- app/Http/Controllers/Admin/Point/V1/Api/MarketController.php
+++ app/Http/Controllers/Admin/Point/V1/Api/MarketController.php
@@ -6,14 +6,51 @@ use App\Http\Controllers\Admin\Point\V1\Models\Market;
 use App\Http\Controllers\Admin\Point\V1\Request\MarketRequest;
 use App\Http\Controllers\Admin\Point\V1\Transformer\MarketTransformer;
 use App\Http\Controllers\Controller;
-use Carbon\Carbon;
+use Illuminate\Http\Request;
 
 class MarketController extends Controller
 {
 
-    public function index(Market $market)
+    public function index(Request $request, Market $market)
     {
-        $markets = $market->paginate(10);
+        $query = $market->query();
+        //场地名称
+        if ($request->has('market_name')) {
+            $query->where('name', 'like', '%' . $request->market_name . '%');
+        }
+
+        //区域
+        if ($request->has('areaid')) {
+            $query->where('areaid', '=', $request->areaid);
+        }
+
+        //场地类型
+        if ($request->has('contract_type')) {
+            $contractType = $request->contract_type;
+            $query->whereHas('contract', function ($query) use ($contractType) {
+                $query->where('type', '=', $contractType);
+            });
+        }
+
+        //合作模式
+        if ($request->has('contract_mode')) {
+            $contractMode = $request->contract_mode;
+            $query->whereHas('contract', function ($query) use ($contractMode) {
+                $query->where('mode', '=', $contractMode);
+            });
+        }
+
+        //场地权限
+        if ($request->has('share_users')) {
+            $shareUsers = explode(',', $request->share_users);
+            $query->whereHas('share', function ($query) use ($shareUsers) {
+                foreach ($shareUsers as $shareUser) {
+                    $query->where("$shareUser", '=', 1);
+                }
+            });
+        }
+
+        $markets = $query->paginate(10);
         return $this->response->paginator($markets, new MarketTransformer());
     }
 
