fc51bcf65164e67c52113e8ba68b76be345624a6
yangyn <920176893@qq.com>
2018-10-22 14:09:22 +0800

    update

---
+++ database/seeds/AddRoleForProcessSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class AddRoleForProcessSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+//        Permission::create(['name' => 'contract', 'display_name' => '合同']);
+        Permission::create(['name' => 'invoice', 'display_name' => '票据']);
+        Permission::create(['name' => 'payments', 'display_name' => '付款']);
+        Permission::create(['name' => 'finance_bill', 'display_name' => '财务开票']);
+        Permission::create(['name' => 'finance_pay', 'display_name' => '财务付款']);
+
+        $legalManager = Role::create(['name' => 'legal-affairs-manager', 'display_name' => '法务主管']);
+        $BDManager = Role::create(['name' => 'bd-manager', 'display_name' => 'BD主管']);
+        $finance = Role::create(['name' => 'finance', 'display_name' => '财务']);
+        $auditor = Role::create(['name' => 'auditor', 'display_name' => '审计']);
+
+        $legalManager->givePermissionTo(['company', 'system', 'project', 'device', 'ad', 'point', 'team', 'report', 'home', 'contract', 'invoice', 'payments']);
+        $BDManager->givePermissionTo(['company', 'system', 'project', 'device', 'ad', 'point', 'team', 'report', 'home', 'contract', 'invoice', 'payments']);
+        $finance->givePermissionTo(['invoice', 'payments']);
+        $auditor->givePermissionTo(['payments']);
+
+        $BD = Role::findByName('user');
+        $BD->givePermissionTo(['contract', 'invoice', 'payments']);
+
+    }
+}
