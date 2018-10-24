c038829332a6d5917397f41f9a73c21dc1562c4e
Joy <joywangx@sina.com>
2018-10-24 14:57:24 +0800

    优惠券规则列表 和优惠券策略

---
+++ xingstation_release/src/page/project/rules/routerView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="rules-root">
+    <router-view/>
+  </div>
+</template>
+<script>
+export default {
+  name: 'RulesView'
+}
+</script>
+
+<style lang="less" scoped>
+.rules-root {
+  height: 100%;
+  margin: 10px;
+  background-color: #eff2f7;
+}
+</style>
