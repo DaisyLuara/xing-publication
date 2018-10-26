c2d1360fa6a55b959782fa1e51dc9e8dffa5039a
Joy <joywangx@sina.com>
2018-10-26 15:19:10 +0800

    两个系统跳转

--- xingstation_release/config/prod.env.js
+++ xingstation_release/config/prod.env.js
@@ -1,7 +1,8 @@
 'use strict'
 module.exports = {
-    NODE_ENV: '"production"',
-    SERVER_URL: '"http://papi.xingstation.com"',
-    HTTPS_SERVER_URL: '"https://papi.xingstation.com"',
-    TOWER_URL: '"http://tower.im/"'
+  NODE_ENV: '"production"',
+  DOMAIN: '"jingfree.top"',
+  SERVER_URL: '"http://papi.xingstation.com"',
+  HTTPS_SERVER_URL: '"https://papi.xingstation.com"',
+  TOWER_URL: '"http://tower.im/"'
 }
