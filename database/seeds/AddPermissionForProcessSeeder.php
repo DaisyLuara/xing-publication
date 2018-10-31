2c8941957abb3251fccd4ccc31e272c8be063073
yangyn <920176893@qq.com>
2018-10-31 14:09:18 +0800

    update

--- database/seeds/AddPermissionForProcessSeeder.php
+++ database/seeds/AddPermissionForProcessSeeder.php
@@ -12,11 +12,20 @@ class AddPermissionForProcessSeeder extends Seeder
      */
     public function run()
     {
-        $users = User::query()->whereIn('phone', ['18301766780', '13764554970', '13601928127'])->get();
-        $users->each(function ($user) {
-            if (!$user->hasPermissionTo('auditing')) {
-                $user->givePermissionTo('auditing');
-            }
-        });
+        //审批法务
+        $legal = User::query()->where('phone', '18301766780')->first();
+        $legal->givePermissionTo('auditing');
+
+        //开票财务
+        $finance_bill = User::query()->where('phone', '13764554970')->first();
+        $finance_bill->givePermissionTo('finance_bill');
+
+        //付款财务
+        $finance_pay = User::query()->where('phone', '13764554970')->first();
+        $finance_pay->givePermissionTo('finance_pay');
+
+        //审批审计
+        $auditor = User::query()->where('phone', '13601928127')->first();
+        $auditor->givePermissionTo('auditing');
     }
 }
