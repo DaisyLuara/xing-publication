9b07273e4677719d48d30808ee3e9c3692df3579
caesar chan <caeasar_chan@live.com>
2018-11-01 14:37:58 +0800

    remove pages from api project

--- xingstation_release/src/store/getters.js
+++
@@ -1,6 +0,0 @@
-const getters = {
-  isRefreshToken: state => state.appSetting.isRefreshToken,
-  currRoute: state => state.appSetting.currRoute,
-  loadingApp: state => state.appSetting.loadingApp
-};
-export default getters
