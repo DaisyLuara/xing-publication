8fc262edd9dfbcd97e19d3fd8cfc71b07acddd80
caesar chan <caeasar_chan@live.com>
2018-11-03 14:02:24 +0800

    update

--- app/Http/Controllers/Admin/Coupon/V1/Api/CouponController.php
+++ app/Http/Controllers/Admin/Coupon/V1/Api/CouponController.php
@@ -29,6 +29,17 @@ class CouponController extends Controller
             $query->where('coupon_batch_id', $request->get('coupon_batch_id'));
         }
 
+        if ($request->has('company_id')) {
+            $companyID = $request->company_id;
+            $query->whereHas('couponBatch', function ($query) use ($companyID) {
+                $query->where('company_id', $companyID);
+            });
+        }
+
+        if ($request->has('start_date') && $request->has('end_date')) {
+            $query->whereBetween('created_at', [$request->get('start_date'), $request->get('end_date')]);
+        }
+
         $coupon = $query->orderByDesc('id')->paginate(10);
 
         return $this->response->paginator($coupon, new CouponTransformer());
