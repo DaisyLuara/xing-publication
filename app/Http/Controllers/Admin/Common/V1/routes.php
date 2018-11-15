f8fe7d36927f5f091f44eaa4ef5d6500f3c43f8d
yangyn <920176893@qq.com>
2018-11-15 17:00:36 +0800

    update

--- app/Http/Controllers/Admin/Common/V1/routes.php
+++ app/Http/Controllers/Admin/Common/V1/routes.php
@@ -83,7 +83,10 @@ $api->version('v1', [
             $api->post('chart_data', 'ChartDataController@chart');//图表
             $api->get('export', 'ExportController@store');//导出
         });
-        $api->get('fcpe', 'ChartDataController@getFCpe');
+
+        $api->get('website/fcpe', 'WebsiteController@getFCpe');
+        $api->post('website/visitor', 'WebsiteController@storeVisitor');
+
     });
 
 });
