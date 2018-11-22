1aa5f6e59cb903e95ac9255f491e137a3328e6d7
yangyn <920176893@qq.com>
2018-11-22 15:40:33 +0800

    update

---
+++ app/Http/Controllers/Admin/Face/V1/Models/XsFacePlayTimes15Permeability.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yangqiang
+ * Date: 2018/11/22
+ * Time: 下午3:02
+ */
+
+namespace App\Http\Controllers\Admin\Face\V1\Models;
+
+
+use App\Models\Model;
+
+class XsFacePlayTimes15Permeability extends Model
+{
+    protected $connection = 'ar';
+    public $table = 'xs_face_playtimes21_permeability';
+}
