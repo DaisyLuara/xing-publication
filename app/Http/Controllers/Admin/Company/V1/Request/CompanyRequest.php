afba39ab7774420480baab74c0235326ab6d870c
caesar chan <caeasar_chan@live.com>
2018-11-28 11:48:38 +0800

    fix bug

--- app/Http/Controllers/Admin/Company/V1/Request/CompanyRequest.php
+++ app/Http/Controllers/Admin/Company/V1/Request/CompanyRequest.php
@@ -18,6 +18,8 @@ class CompanyRequest extends FormRequest
                 return [
                     'name' => 'required|string',
                     'address' => 'required|string',
+                    'phone' => 'filled|exists:customer,phone',
+                    'telephone' => 'filled|exists:customer,telephone',
                 ];
                 break;
             case 'PATCH':
