bf3b5d7362d2dac8cf50120a87f057a2f601ec77
yangyn <920176893@qq.com>
2018-11-28 09:42:01 +0800

    update

--- database/seeds/AddPermissionAndRoleForTeamSeeder.php
+++ database/seeds/AddPermissionAndRoleForTeamSeeder.php
@@ -12,11 +12,14 @@ class AddPermissionAndRoleForTeamSeeder extends Seeder
      */
     public function run()
     {
-        $tester=Role::create(['name' => 'tester', 'display_name' => '测试']);
-        $designer=Role::create(['name'=>'designer','display_name'=>'设计']);
+//        $tester = Role::create(['name' => 'tester', 'display_name' => '测试']);
+//        $designer = Role::create(['name' => 'designer', 'display_name' => '设计']);
+//
+//        $tester->givePermissionTo(['company', 'project', 'device', 'ad', 'point', 'team', 'report', 'home', 'contract', 'invoice', 'payments']);
+//        $designer->givePermissionTo(['home', 'team']);
 
-        $tester->givePermissionTo(['company', 'project', 'device', 'ad', 'point', 'team', 'report', 'home', 'contract', 'invoice', 'payments']);
-        $designer->givePermissionTo(['home','team']);
+        $projectMa = Role::findByName('project-manager');
+        $projectMa->givePermissionTo(['team']);
 
     }
 }
