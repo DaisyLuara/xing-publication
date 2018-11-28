6bca61b602f9414b60b5a5ff02b9b4157caf1a87
yangyn <920176893@qq.com>
2018-11-28 13:32:39 +0800

    update

--- app/Http/Controllers/Admin/Invoice/V1/Transformer/InvoiceContentTransformer.php
+++ app/Http/Controllers/Admin/Invoice/V1/Transformer/InvoiceContentTransformer.php
@@ -27,6 +27,9 @@ class InvoiceContentTransformer extends TransformerAbstract
 
     public function includeInvoiceKind(InvoiceContent $invoiceContent)
     {
+        if ($invoiceContent->invoiceKind) {
+            return null;
+        }
         return $this->item($invoiceContent->invoiceKind, new InvoiceKindTransformer());
     }
 
