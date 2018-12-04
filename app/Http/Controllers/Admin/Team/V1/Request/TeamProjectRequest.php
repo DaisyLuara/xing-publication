44e32c5cea3dc88398dc2a4fc6362c84759c0e32
yangyn <920176893@qq.com>
2018-12-04 13:51:04 +0800

    update

--- app/Http/Controllers/Admin/Team/V1/Request/TeamProjectRequest.php
+++ app/Http/Controllers/Admin/Team/V1/Request/TeamProjectRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin\Team\V1\Request;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class TeamProjectRequest extends FormRequest
 {
@@ -21,16 +23,32 @@ class TeamProjectRequest extends FormRequest
      *
      * @return array
      */
-    public function rules()
+    public function rules(Request $request)
     {
         switch ($this->method()) {
             case 'POST':
                 return [
-
+                    'project_name' => 'required',
+                    'belong' => 'required|unique:team_projects',
+                    'project_attribute' => Rule::in([1, 2, 3, 4]),
+                    'link_attribute' => Rule::in([0, 1]),
+                    'h5_attribute' => Rule::in([1, 2]),
+                    'xo_attribute' => Rule::in([0, 1]),
+                    'remark' => 'max:150',
+                    'type' => Rule::in([0, 1]),
+                    'member.*.rate'=>'numeric'
                 ];
                 break;
             case 'PATCH':
                 return [
+                    'belong' => Rule::unique('team_projects')->ignore($request->id),
+                    'project_attribute' => Rule::in([1, 2, 3, 4]),
+                    'link_attribute' => Rule::in([0, 1]),
+                    'h5_attribute' => Rule::in([1, 2]),
+                    'xo_attribute' => Rule::in([0, 1]),
+                    'remark' => 'max:150',
+                    'type' => Rule::in([0, 1]),
+                    'member.*.rate'=>'numeric'
                 ];
                 break;
             default:
