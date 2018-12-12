6764d3746581a337428b8a3add26d731e30e0bf4
caesar chan <caeasar_chan@live.com>
2018-12-12 20:14:56 +0800

    绑定猫酷ID‘

--- app/Http/Controllers/Admin/MallCoo/V1/Api/UserController.php
+++ app/Http/Controllers/Admin/MallCoo/V1/Api/UserController.php
@@ -3,11 +3,10 @@
 namespace App\Http\Controllers\Admin\MallCoo\V1\Api;
 
 use App\Http\Controllers\Admin\WeChat\V1\Models\ThirdPartyUser;
-use App\Http\Controllers\Admin\ShortUrl\V1\Models\ShortUrl;
-use function GuzzleHttp\Psr7\parse_query;
+use App\Models\WeChatUser;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Hashids\Hashids;
+use DB;
 
 
 class UserController extends Controller
@@ -15,10 +14,13 @@ class UserController extends Controller
     public function oauth(Request $request)
     {
         $this->validate($request, [
-            'redirect_url' => 'required|url'
+            'redirect_url' => 'required|url',
+            'sign' => 'required',
         ]);
 
+        $userID = decrypt($request->sign);
         $redirect_url = $request->get('redirect_url');
+        $redirect_url = add_query_string($redirect_url, 'user_id', $userID);
 
         $mall_coo = app('mall_coo');
         $callback_url = 'http://' . $request->getHost() . '/api/mallcoo/user/callback?redirect_url=' . urlencode(($redirect_url));
@@ -28,49 +30,50 @@ class UserController extends Controller
 
     public function callback(Request $request)
     {
-        $redirect_url = urldecode($request->get('redirect_url'));
 
         $ticket = $request->get('Ticket');
-
-        $queries = parse_query(parse_url($redirect_url, PHP_URL_QUERY), false);
 
         //获取用户UserToken
         $mall_coo = app('mall_coo');
         $result = $mall_coo->getTokenByTicket($ticket);
-
-        if ($result['Code'] !== 1) {
-            return $result['Message'];
-        }
+        abort_if($result['Code'] !== 1, 500, result['Message']);
 
         //获取会员信息
         $response = $mall_coo->getUserInfoByOpenUserID($result['Data']['OpenUserId']);
-        if ($response['Code'] !==1) {
-            return $response['Message'];
-        }
+        abort_if($result['Code'] !== 1, 500, result['Message']);
 
-        $userInfo          = $response['Data'];
-        $openUserId        = $userInfo['OpenUserID'];
-        $mobile            = $userInfo['Mobile'];
-        $mallCooWxOpenId   = $userInfo['WXOpenID'];
-        $username          = $userInfo['UserName'];
-        $gender            = $userInfo['Gender'];
-        $birthday          = $userInfo['Birthday'];
+        $userInfo = $response['Data'];
+        $openUserId = $userInfo['OpenUserID'];
+        $mobile = $userInfo['Mobile'];
+        $mallCooWxOpenId = $userInfo['WXOpenID'];
+        $username = $userInfo['UserName'];
+        $gender = $userInfo['Gender'];
+        $birthday = $userInfo['Birthday'];
         $mallCardApplyTime = $userInfo['MallCardApplyTime'];
 
-        $separator = strpos($redirect_url, '?') ? '&' : '?';
-        $redirect_url = $redirect_url . $separator . 'open_user_id=' . $openUserId;
+        $redirect_url = urldecode($request->get('redirect_url'));
+        $redirect_url = add_query_string($redirect_url, 'open_user_id', $openUserId);
 
-        $thirdPartyUser = ThirdPartyUser::updateOrCreate(
-            ['mallcoo_open_user_id' => $openUserId],
-            [
-                'mobile' => $mobile,
-                'username' => $username,
-                'mallcoo_wx_open_id' => $mallCooWxOpenId,
-                'gender' => $gender,
-                'birthday' => $birthday,
-                'mall_card_apply_time' => $mallCardApplyTime,
-            ]
-        );
+        DB::beginTransaction();
+        try {
+
+            $thirdPartyUser = ThirdPartyUser::updateOrCreate(
+                ['mallcoo_open_user_id' => $openUserId],
+                [
+                    'mobile' => $mobile,
+                    'username' => $username,
+                    'mallcoo_wx_open_id' => $mallCooWxOpenId,
+                    'gender' => $gender,
+                    'birthday' => $birthday,
+                    'mall_card_apply_time' => $mallCardApplyTime,
+                ]
+            );
+            WeChatUser::query()->update(['id' => $request->user_id], ['mallcoo_open_user_id' => $thirdPartyUser->id]);
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollback();//事务回滚
+            abort(500, $e->getMessage());
+        }
 
         return redirect($redirect_url);
     }
@@ -89,14 +92,13 @@ class UserController extends Controller
         $sUrl = 'https://openapi10.mallcoo.cn/Shop/V1/GetList/';
 
         $data = [
-              "PageIndex" => 1,
-              "PageSize" => null,
-              "FloorID" => null,
-              "CommercialTypeID" => null,
+            "PageIndex" => 1,
+            "PageSize" => null,
+            "FloorID" => null,
+            "CommercialTypeID" => null,
         ];
 
         $result = $mall_coo->send($sUrl, $data);
-        dd($result);
         abort_if($result['Code'] != 1, 500, $result['Message']);
 
         return response()->json($result['Data']);
--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -352,3 +352,16 @@ function arrayMultiSearch($needle, $haystack)
     return false;
 }
 
+if (!function_exists('add_query_string')) {
+    function add_query_string($url, $key, $value)
+    {
+        $url = preg_replace('/(.*)(?|&)' . $key . '=[^&]+?(&)(.*)/i', '$1$2$4', $url . '&');
+        $url = substr($url, 0, -1);
+        if (strpos($url, '?') === false) {
+            return ($url . '?' . $key . '=' . $value);
+        } else {
+            return ($url . '&' . $key . '=' . $value);
+        }
+    }
+}
+
