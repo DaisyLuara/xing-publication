99327a8bf21c261c8071a429503bd2a05198b55f
sylvia <xinyu_yuan@163.com>
2018-12-14 13:17:44 +0800

    优惠券导出优化：通用点位不显示、倒序、抑制科学计数

--- app/Exports/CouponExport.php
+++ app/Exports/CouponExport.php
@@ -4,11 +4,13 @@ namespace App\Exports;
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Events\AfterSheet;
 use PhpOffice\PhpSpreadsheet\Style\Alignment;
 use PhpOffice\PhpSpreadsheet\Style\Border;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
 
-class CouponExport extends AbstractExport
+class CouponExport extends AbstractExport implements ShouldAutoSize
 {
 
     public function __construct($request)
@@ -30,15 +32,15 @@ class CouponExport extends AbstractExport
             ->leftJoin('customers', 'coupons.shop_customer_id', '=', 'customers.id');
 
         if (!is_null($this->status)) {
-            $query->where('coupons.status','=', $this->status);
+            $query->where('coupons.status', '=', $this->status);
         }
 
         if (!is_null($this->couponBatchId)) {
-            $query->where('coupons.coupon_batch_id','=',  $this->couponBatchId);
+            $query->where('coupons.coupon_batch_id', '=', $this->couponBatchId);
         }
 
         if (!is_null($this->companyId)) {
-            $query->where('coupon_batches.company_id','=',  $this->companyId);
+            $query->where('coupon_batches.company_id', '=', $this->companyId);
         }
 
         if (!is_null($this->startDate) && !is_null($this->endDate)) {
@@ -54,7 +56,7 @@ class CouponExport extends AbstractExport
             $query->where('coupon_batches.bd_user_id', '=', $loginUser->id);
         }
 
-        $query = $query->selectRaw("coupons.code as '编码',coupon_batches.name as '名称',
+        $query = $query->selectRaw("concat('\t',coupons.code,'\t') as 'code',coupon_batches.name as '名称',
                        case coupons.status
                        when 0 then '未领取'
                        when 1 then '已使用'
@@ -70,22 +72,25 @@ class CouponExport extends AbstractExport
                   coupons.oid as 'point',
                   customers.name as '核销人'");
 
-        $points = $query->get()
+        $points = $query->orderBy('coupons.id', 'desc')->get()
             ->map(function ($value) {
-                $point_name = DB::connection('ar')->table('avr_official')
-                    ->leftJoin('avr_official_market','avr_official.marketid','=','avr_official_market.marketid')
-                    ->leftJoin('avr_official_area','avr_official_market.areaid','avr_official_area.areaid')
-                    ->where('avr_official.oid','=',$value->point)
-                    ->selectRaw("concat(avr_official_area.name,'-',avr_official_market.name,'-',avr_official.name) as 'point_name'")->value('point_name');
-                $value->point = $point_name;
-
+                if ($value->point > 0) {
+                    $point_name = DB::connection('ar')->table('avr_official')
+                        ->leftJoin('avr_official_market', 'avr_official.marketid', '=', 'avr_official_market.marketid')
+                        ->leftJoin('avr_official_area', 'avr_official_market.areaid', 'avr_official_area.areaid')
+                        ->where('avr_official.oid', '=', $value->point)
+                        ->selectRaw("concat(avr_official_area.name,'-',avr_official_market.name,'-',avr_official.name) as 'point_name'")->value('point_name');
+                    $value->point = $point_name;
+                } else {
+                    $value->point = '';
+                }
                 return (array)$value;
 
-        })->toArray();
+            })->toArray();
 
         $header = ['编码', '名称', '状态', '手机号', '微信ID', '淘宝ID', '创建时间', '核销时间', '有效期', ' 公司', '点位', '核销人'];
         $this->headerNum = count($header);
-        array_unshift($points,$header);
+        array_unshift($points, $header);
         $data = collect($points);
         $this->data = collect($data);
 
@@ -106,6 +111,9 @@ class CouponExport extends AbstractExport
                         ]
                     ]
                 ]);
+
+                //编码被科学计数问题
+                $event->sheet->getStyle('A1:A' . $this->data->count())->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_TEXT);
 
                 //水平居中 垂直居中
                 $event->sheet->getDelegate()
