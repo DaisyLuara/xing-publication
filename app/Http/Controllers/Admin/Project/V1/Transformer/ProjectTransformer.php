a1d484d2066e9533a5dc3f5e199bc4431910f355
sylvia <xinyu_yuan@163.com>
2018-12-25 14:46:22 +0800

    bug fix

--- app/Http/Controllers/Admin/Project/V1/Transformer/ProjectTransformer.php
+++ app/Http/Controllers/Admin/Project/V1/Transformer/ProjectTransformer.php
@@ -15,6 +15,7 @@ class ProjectTransformer extends TransformerAbstract
     public function transform(Project $project)
     {
         $teamProject = TeamProject::where("belong", $project->versionname)->first();
+
         $member = [];
         if ($teamProject) {
             $member = $teamProject->member->toArray();
@@ -34,8 +35,8 @@ class ProjectTransformer extends TransformerAbstract
             'created_at' => $project->date,
             'policy_id' => $project->policy_id,
             'updated_at' => formatClientDate($project->clientdate),
-            'tester' => ($member && $member['tester']) ? implode(',', array_column($member['tester'], 'user_name')) : null,
-            'operation' => ($member && $member['operation']) ? implode(',', array_column($member['operation'], 'user_name')) : null
+            'tester' => ($member && isset($member['tester'])) ? implode(',', array_column($member['tester'] ?? [], 'user_name')) : null,
+            'operation' => ($member && isset($member['operation'])) ? implode(',', array_column($member['operation'] ?? [], 'user_name')) : null
         ];
     }
 
