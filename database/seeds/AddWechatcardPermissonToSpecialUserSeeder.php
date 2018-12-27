a2c98c3caac98009fa6e75b724707907ead42663
Dirklow <dirklow23@gmail.com>
2018-12-27 10:50:41 +0800

    update seeder

--- database/seeds/AddWechatcardPermissonToSpecialUserSeeder.php
+++ database/seeds/AddWechatcardPermissonToSpecialUserSeeder.php
@@ -16,7 +16,7 @@ class AddWechatcardPermissonToSpecialUserSeeder extends Seeder
     {
         Permission::create(['name' => 'wechat_card', 'display_name' => '微信卡券']);
 
-        $users = User::query()->whereIn('name', ['方圆', '陈重', '赵俊', '刘洪亚', '张诗瑶','测试'])->get();
+        $users = User::query()->whereIn('name', ['方圆', '陈重', '赵俊', '刘洪亚', '张诗瑶'])->get();
         foreach ($users as $user) {
             $user->givePermissionTo('wechat_card');
         }
