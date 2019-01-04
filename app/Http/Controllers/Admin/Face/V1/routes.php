0dddb4a10f80ae1d2867d421a30724e50a7c5c75
yangyn <920176893@qq.com>
2019-01-04 12:00:02 +0800

    update

--- app/Http/Controllers/Admin/Face/V1/routes.php
+++ app/Http/Controllers/Admin/Face/V1/routes.php
@@ -9,6 +9,7 @@ $api->version('v1', [
         'expires' => config('api.rate_limits.access.expires'),
     ], function ($api) {
         $api->group(['middleware' => "api.auth", 'model' => 'App\Models\User'], function ($api) {
+            $api->get('timestamp', 'FaceCountController@timestamp');
         });
     });
 
