138c19492122e763d6d63abf9bc4906fc358910c
yangyn <920176893@qq.com>
2019-01-11 15:56:28 +0800

    update

---
+++ database/migrations/2019_01_11_145308_create_process_staff_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Http\Controllers\Admin\Privilege\V1\Models\Role;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProcessStaffTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('process_staffs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('role');
+            $table->string('line')->comment('contract,invoice,payment');
+        });
+        $legalRole = Role::findByName('legal-affairs');
+        $legal = $legalRole->users()->first();
+        DB::table('process_staffs')->insert(['user_id' => $legal->id, 'role' => 'legal-affairs', 'line' => 'contract']);
+        DB::table('process_staffs')->insert(['user_id' => $legal->id, 'role' => 'legal-affairs', 'line' => 'invoice']);
+        DB::table('process_staffs')->insert(['user_id' => $legal->id, 'role' => 'legal-affairs', 'line' => 'payment']);
+
+        $financeRole = Role::findByName('finance');
+        $finance = $financeRole->users()->first();
+        DB::table('process_staffs')->insert(['user_id' => $finance->id, 'role' => 'finance', 'line' => 'invoice']);
+        DB::table('process_staffs')->insert(['user_id' => $finance->id, 'role' => 'finance', 'line' => 'payment']);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('process_staffs');
+    }
+}
