21ba001d6dbf9687354247649c3479b103ab0efe
Joy <joy@fdvdeMacBook-Pro.local>
2019-01-15 17:35:04 +0800

    fix

---
+++ database/migrations/2019_01_14_135307_alter_warehouse_changes.php
@@ -0,0 +1,32 @@
+<?php
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+class AlterWarehouseChanges extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('erp_warehouse_changes', function (Blueprint $table) {
+            $table->dropColumn('sku');
+            $table->integer('product_id')->after('id')->nullable()->comment('产品ID');
+            $table->string('remark', 255)->nullable()->comment('调拨记录备注')->change();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('erp_warehouse_changes', function (Blueprint $table) {
+            $table->string('sku')->nullable()->comment('产品SKU');
+            $table->string('remark', 255)->nullable()->comment('调拨记录备注');
+        });
+    }
+}
