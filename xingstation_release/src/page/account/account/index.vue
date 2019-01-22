254f16b55b6714bf9cd29d0e44b5711eb466b427
Joy <joy@fdvdeMacBook-Pro.local>
2019-01-22 09:39:06 +0800

    账号管理修改

--- xingstation_release/src/page/account/account/index.vue
+++ xingstation_release/src/page/account/account/index.vue
@@ -1,52 +1,22 @@
 <template>
-  <div 
-    class="root">
-    <div 
-      class="account-wrap">
-      <div 
-        class="item-info">
-        <el-form 
-          ref="userForm" 
-          :model="userForm" 
-          :rules="rules" 
-          label-width="80px">
-          <el-form-item 
-            label="姓名" 
-            prop="user.name">
-            <el-input  
-              v-model="userForm.user.name" 
-              :maxlength="10" 
-              class="user-form-input"/>
+  <div class="root">
+    <div class="account-wrap">
+      <div class="item-info">
+        <el-form ref="userForm" :model="userForm" :rules="rules" label-width="80px">
+          <el-form-item label="姓名" prop="user.name">
+            <el-input v-model="userForm.user.name" :maxlength="10" class="user-form-input"/>
           </el-form-item>
-          <el-form-item 
-            label="手机号码"
-            prop="user.phone">
-            <el-input  
-              v-model="userForm.user.phone" 
-              :maxlength="11" 
-              class="user-form-input"/>
+          <el-form-item label="手机号码" prop="user.phone">
+            <el-input v-model="userForm.user.phone" :maxlength="11" class="user-form-input"/>
           </el-form-item>
-          <el-form-item 
-            label="密码" 
-            prop="user.password">
-            <el-input 
-              v-model="userForm.user.password" 
-              type="password" 
-              class="user-form-input" />
+          <el-form-item label="密码" prop="user.password">
+            <el-input v-model="userForm.user.password" type="password" class="user-form-input"/>
           </el-form-item>
-          <el-form-item 
-            label="确认密码" 
-            prop="user.repassword">
-            <el-input  
-              v-model="userForm.user.repassword" 
-              type="password" 
-              class="user-form-input"/>
+          <el-form-item label="确认密码" prop="user.repassword">
+            <el-input v-model="userForm.user.repassword" type="password" class="user-form-input"/>
           </el-form-item>
           <el-form-item>
-            <el-button 
-              :loading="loading" 
-              type="primary" 
-              @click="onSubmit('userForm')">保存</el-button>
+            <el-button :loading="loading" type="primary" @click="onSubmit('userForm')">保存</el-button>
           </el-form-item>
         </el-form>
       </div>
@@ -55,10 +25,10 @@
 </template>
 
 <script>
-const USERINFO_API = '/api/user?include=permissions'
+const DOMAIN = process.env.DOMAIN;
 
-import auth from 'service/auth'
-import router from 'router'
+import auth from "service/auth";
+import router from "router";
 import {
   Row,
   Col,
@@ -70,134 +40,137 @@ import {
   CheckboxGroup,
   RadioGroup,
   Radio
-} from 'element-ui'
+} from "element-ui";
 
 export default {
-  name: 'Account',
+  name: "Account",
   components: {
-    'el-button': Button,
-    'el-input': Input,
-    'el-form': Form,
-    'el-form-item': FormItem
+    "el-button": Button,
+    "el-input": Input,
+    "el-form": Form,
+    "el-form-item": FormItem
   },
   data() {
     return {
       setting: {
         isOpenSelectAll: true,
         loading: false,
-        loadingText: '拼命加载中'
+        loadingText: "拼命加载中"
       },
       userForm: {
         user: {
-          name: '',
-          phone: '',
-          password: '',
-          repassword: ''
+          name: "",
+          phone: "",
+          password: "",
+          repassword: ""
         }
       },
       rules: {
-        'user.phone': [
+        "user.phone": [
           {
             validator: (rule, value, callback) => {
               if (/^\s*$/.test(value)) {
-                callback('请输入手机')
+                callback("请输入手机");
               } else if (!/^1[3456789]\d{9}$/.test(value)) {
-                callback('手机格式不正确,请重新输入')
+                callback("手机格式不正确,请重新输入");
               } else {
-                callback()
+                callback();
               }
             },
-            trigger: 'blur'
+            trigger: "submit",
+            required: true
           }
         ],
-        'user.name': [{ message: '请输入姓名', trigger: 'blur' }],
-        'user.password': [
+        "user.name": [
+          { message: "请输入姓名", trigger: "submit", required: true }
+        ],
+        "user.password": [
           {
             validator: (rule, value, callback) => {
-              if (/^\s*$/.test(value)) {
-                callback('请输入密码')
-              } else if (!/^.{6,20}$/.test(value)) {
-                callback('密码长度不正确,请重新输入')
+              if (!/^.{6,20}$/.test(value) && value !== "") {
+                callback("密码长度不正确,请重新输入");
               } else {
-                callback()
+                callback();
               }
             },
-            trigger: 'blur'
+            trigger: "submit"
           }
         ],
-        'user.repassword': [
+        "user.repassword": [
           {
             validator: (rule, value, callback) => {
-              if (value === '') {
-                callback(new Error('请再次输入密码'))
-              } else if (value !== this.userForm.user.password) {
-                callback(new Error('两次输入密码不一致!'))
+              if (value !== this.userForm.user.password) {
+                callback(new Error("两次输入密码不一致!"));
               } else {
-                callback()
+                callback();
               }
             },
-            trigger: 'blur'
+            trigger: "submit"
           }
         ]
       },
       loading: false
-    }
+    };
   },
   created: function() {
     if (this.setting.loading == true) {
-      return false
+      return false;
     }
-    this.setting.loadingText = '拼命加载中'
-    let user = JSON.parse(this.$cookie.get('user_info'))
-    this.userForm.user.name = user.name
-    this.userForm.user.phone = user.phone
-    this.setting.loading = false
+    this.setting.loadingText = "拼命加载中";
+    let user = JSON.parse(this.$cookie.get("user_info"));
+    this.userForm.user.name = user.name;
+    this.userForm.user.phone = user.phone;
+    this.setting.loading = false;
   },
   methods: {
     onSubmit(formName) {
       this.$refs[formName].validate(valid => {
-        if (!this.userForm.user.password && !this.userForm.user.repassword) {
-          delete this.rules['user.password']
-          delete this.rules['user.repassword']
-          delete this[formName].user.password
-          valid = true
-        }
         if (valid) {
-          this.loading = true
-          delete this[formName].user.repassword
+          if (this[formName].user.password === "") {
+            delete this[formName].user.password;
+          }
+          this.loading = true;
+          delete this[formName].user.repassword;
           auth
             .modifyUser(this, this[formName].user)
             .then(result => {
-              this.loading = false
-              let user_info = JSON.parse(this.$cookie.get('user_info'))
-              user_info.name = result.name
-              user_info.phone = result.phone
-              user_info.password = ''
-              this.$store.commit('setCurUserInfo', user_info)
-              this.$cookie.set('user_info', JSON.stringify(user_info))
+              this.loading = false;
+              let userInfo = {
+                id: result.id,
+                name: result.name,
+                created_at: result.created_at,
+                ar_user_id: result.ar_user_id,
+                bind_weixin: result.bind_weixin,
+                roles: result.roles,
+                phone: result.phone
+              };
+              this.$store.commit("setCurUserInfo", userInfo);
+              this.$cookie.set("user_info", JSON.stringify(userInfo), {
+                domain: DOMAIN
+              });
               this.$message({
-                message: '修改成功',
-                type: 'success'
-              })
+                message: "修改成功",
+                type: "success"
+              });
             })
             .catch(error => {
-              this.loading = false
-              console.log(error)
-            })
+              this.loading = false;
+              console.log(error);
+            });
         } else {
-          return
+          return;
         }
-      })
+      });
     },
 
     resetForm(formName) {
-      this.$refs[formName].resetFields()
+      this.$refs[formName].resetFields();
     },
     historyBack() {
-      router.back()
+      router.back();
     }
   }
-}
+};
 </script>
 <style scoped lang="less">
 .root {
