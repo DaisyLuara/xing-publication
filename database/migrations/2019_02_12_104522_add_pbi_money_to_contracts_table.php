b3a1c9d37729c7d1acd549fd85a72773bea7a30b
sylvia <xinyu_yuan@163.com>
2019-02-18 15:37:41 +0800

    数据库字段大小

--- database/migrations/2019_02_12_104522_add_pbi_money_to_contracts_table.php
+++ database/migrations/2019_02_12_104522_add_pbi_money_to_contracts_table.php
@@ -14,7 +14,7 @@ class AddPbiMoneyToContractsTable extends Migration
     public function up()
     {
         Schema::table('contracts', function (Blueprint $table) {
-            $table->decimal('pbi_money')->nullable()->default(null)->comment('pbi奖金总数，即：合同收款金额-费用');
+            $table->decimal('pbi_money',12,2)->nullable()->default(null)->comment('pbi奖金总数，即：合同收款金额-费用');
             $table->timestamp('pbi_date')->nullable()->default(null)->comment('pbi奖金发放时间');
         });
     }
