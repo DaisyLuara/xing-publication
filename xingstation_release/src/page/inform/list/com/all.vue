1c2e90b7e9a1b20ddd3d018e08d351c189c66524
Joy Wang <joywangx@sina.com>
2019-03-01 09:37:34 +0800

    delete

--- xingstation_release/src/page/inform/list/com/all.vue
+++ xingstation_release/src/page/inform/list/com/all.vue
@@ -1,63 +1,41 @@
 <template>
-  <div 
+  <div
     v-loading="setting.loading"
-    :element-loading-text="setting.loadingText" 
-    class="page-list-template tab">
-    <div 
-      class="actions-wrap">
-      <el-button 
-        v-if="unreadCount !== 0"
-        size="small"
-        type="info"
-        @click="readNotifications" >全部读取</el-button>
+    :element-loading-text="setting.loadingText"
+    class="page-list-template tab"
+  >
+    <div class="actions-wrap">
+      <el-button v-if="unreadCount !== 0" size="small" type="info" @click="readNotifications">全部读取</el-button>
     </div>
-    <div 
-      class="table-area">
-      <el-table
-        :data="noticeList">
-        <el-table-column
-          prop="id"
-          label="ID">
-          <template 
-            slot-scope="scope">
-            {{ scope.row.data.id }}
+    <div class="table-area">
+      <el-table :data="noticeList">
+        <el-table-column prop="id" label="ID">
+          <template slot-scope="scope">{{ scope.row.data.id }}</template>
+        </el-table-column>
+        <el-table-column prop="reply_content" label="内容">
+          <template slot-scope="scope">{{ scope.row.data.reply_content }}</template>
+        </el-table-column>
+        <el-table-column prop="user_name" label="创建人">
+          <template slot-scope="scope">{{ scope.row.data.user_name }}</template>
+        </el-table-column>
+        <el-table-column prop="created_at" label="创建时间"/>
+        <el-table-column prop="read_at" label="读取时间"/>
+        <el-table-column label="操作" width="150">
+          <template slot-scope="scope">
+            <el-button size="small" type="danger" @click="deleteInfo(scope.row)">删除</el-button>
           </template>
         </el-table-column>
-        <el-table-column
-          prop="reply_content"
-          label="内容">
-          <template 
-            slot-scope="scope">
-            {{ scope.row.data.reply_content }}
-          </template>
-        </el-table-column>
-        <el-table-column
-          prop="user_name"
-          label="创建人">
-          <template 
-            slot-scope="scope">
-            {{ scope.row.data.user_name }}
-          </template>
-        </el-table-column>
-        <el-table-column
-          prop="created_at"
-          label="创建时间"
-        />
-        <el-table-column
-          prop="read_at"
-          label="读取时间"
-        />
       </el-table>
     </div>
 
-    <div 
-      class="pagination">
+    <div class="pagination">
       <el-pagination
         :current-page.sync="pagination.currentPage"
         :page-size="pagination.pageSize"
         :total="pagination.total"
         layout="prev, pager, next, jumper"
-        @current-change="handleCurrentChange"/>
+        @current-change="handleCurrentChange"
+      />
     </div>
   </div>
 </template>
@@ -70,9 +48,14 @@ import {
   TableColumn,
   Pagination,
   Button
-} from 'element-ui'
-import { getNoticeList, notificationStats, readNotifications } from 'service'
-import { mapState } from 'vuex'
+} from "element-ui";
+import {
+  getNoticeList,
+  notificationStats,
+  readNotifications,
+  deleteNotifications
+} from "service";
+import { mapState } from "vuex";
 export default {
   components: {
     ElSelect: Select,
@@ -85,7 +68,7 @@ export default {
   props: {
     active: {
       type: String,
-      default: 'all'
+      default: "all"
     }
   },
   data() {
@@ -98,11 +81,11 @@ export default {
       },
       setting: {
         loading: true,
-        loadingText: '拼命加载中'
+        loadingText: "拼命加载中"
       },
       noticeList: [],
       loading: false
-    }
+    };
   },
   computed: {
     ...mapState({
@@ -113,68 +96,100 @@ export default {
   },
   watch: {
     lastClickTab: function() {
-      if (this.lastClickTab === 'all') {
-        this.processState()
+      if (this.lastClickTab === "all") {
+        this.processState();
       }
     }
   },
   mounted() {
-    this.processState()
+    this.processState();
   },
   created() {
-    this.getNoticeList()
-    this.unreadCount = this.$store.state.notificationCount.noticeCount
+    this.getNoticeList();
+    this.unreadCount = this.$store.state.notificationCount.noticeCount;
   },
   methods: {
+    deleteNotifications(users) {
+      let ids = [users.id];
+      MessageBox.confirm("确认删除选中信息?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(() => {
+          this.setting.loadingText = "删除中";
+          this.setting.loading = true;
+          deleteNotification(this, id)
+            .then(response => {
+              this.setting.loading = false;
+              this.$message({
+                type: "success",
+                message: "删除成功！"
+              });
+              this.pagination.currentPage = 1;
+              this.getNoticeList();
+            })
+            .catch(error => {
+              this.$message({
+                type: "warning",
+                message: error.response.data.message
+              });
+              this.setting.loading = false;
+            });
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
     getNoticeList() {
-      this.setting.loading = true
-      let pageNum = this.pagination.currentPage
+      this.setting.loading = true;
+      let pageNum = this.pagination.currentPage;
       let args = {
         page: pageNum
-      }
-      this.setting.loadingText = '拼命加载中'
+      };
+      this.setting.loadingText = "拼命加载中";
       getNoticeList(this, args)
         .then(response => {
-          this.noticeList = response.data
-          this.pagination.total = response.meta.pagination.total
-          this.setting.loading = false
+          this.noticeList = response.data;
+          this.pagination.total = response.meta.pagination.total;
+          this.setting.loading = false;
         })
         .catch(error => {
-          this.setting.loading = false
-        })
+          this.setting.loading = false;
+        });
     },
     notificationStats() {
       notificationStats(this)
         .then(response => {
-          this.$store.commit('saveNotificationState', response)
-          this.getNoticeList()
+          this.$store.commit("saveNotificationState", response);
+          this.getNoticeList();
         })
         .catch(err => {
-          console.log(err)
-        })
+          console.log(err);
+        });
     },
     readNotifications() {
       readNotifications(this)
         .then(response => {
-          this.notificationStats()
+          this.notificationStats();
         })
         .catch(err => {
-          console.log(err)
-        })
+          console.log(err);
+        });
     },
     processState() {
-      if (this.lastClickTab === 'all' || this.lastClickTab === '') {
+      if (this.lastClickTab === "all" || this.lastClickTab === "") {
         this.currentPage =
-          this.lastPage !== null ? this.lastPage : this.currentPage
+          this.lastPage !== null ? this.lastPage : this.currentPage;
       }
     },
 
     handleCurrentChange(e) {
-      this.currentPage = e
-      this.getNoticeList()
+      this.currentPage = e;
+      this.getNoticeList();
     }
   }
-}
+};
 </script>
 <style lang="less" scoped>
 .page-list-template {
