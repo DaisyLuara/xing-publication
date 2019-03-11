c2b71a831e140c31f713b9487596114b42043801
sylvia <xinyu_yuan@163.com>
2019-03-11 14:07:21 +0800

    去掉对 dev.env.js 的修改

--- xingstation_release/config/dev.env.js
+++ xingstation_release/config/dev.env.js
@@ -2,9 +2,9 @@ var merge = require('webpack-merge')
 var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-    NODE_ENV: '"development"',
-    DOMAIN: '"jingfree.top"',
-    SERVER_URL: '"http://papi.jingfree.top"',
-    HTTPS_SERVER_URL: '"http://papi.jingfree.top"',
-    TOWER_URL: '"/tower/"'
+  NODE_ENV: '"development"',
+  DOMAIN: '"jingfree.top"',
+  SERVER_URL: '"http://papi.jingfree.top"',
+  HTTPS_SERVER_URL: '"http://papi.jingfree.top"',
+  TOWER_URL: '"/tower/"'
 })
