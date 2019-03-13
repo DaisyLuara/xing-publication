534a65e75cab96c0c964b3835eaa235c854cae95
yangyn <920176893@qq.com>
2019-03-13 17:42:22 +0800

    update

--- app/Http/Controllers/Admin/Report/V1/Api/TodayDataController.php
+++ app/Http/Controllers/Admin/Report/V1/Api/TodayDataController.php
@@ -188,10 +188,10 @@ class TodayDataController extends Controller
     public function getAreaDistribution($request, Builder $query)
     {
         $total = XsFaceCountToday::query()->selectRaw("sum(looktimes) as num")->first()->toArray();
-        $case1 = "when oid=20 or oid=30 then 'A' ";
-        $case2 = "when oid=40 or oid=50 then 'B' ";
-        $case3 = "when oid=60 or oid=70 then 'C' ";
-        $case4 = "when oid=80 or oid=90 then 'D' ";
+        $case1 = "when oid=739 or oid=740 or oid=741 then 'A' ";
+        $case2 = "when oid=742 or oid=743 or oid=744 then 'B' ";
+        $case3 = "when oid=745 or oid=746 or oid=747 then 'C' ";
+        $case4 = "when oid=748 then 'D' ";
         $sql = $case1 . $case2 . $case3 . $case4;
 
         $date = Carbon::now()->toDateString();
