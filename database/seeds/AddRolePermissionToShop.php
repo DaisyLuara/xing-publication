5814d6736cf88378ec0d6a905fb600ea94a64196
fanzhuo <fans1992@qq.com>
2019-03-25 11:41:08 +0800

    permission fix

--- database/seeds/AddRolePermissionToShop.php
+++ database/seeds/AddRolePermissionToShop.php
@@ -23,9 +23,8 @@ class AddRolePermissionToShop extends Seeder
                 ['name' => 'shop_project.project', 'display_name' => '标准节目'],
             ]],
             ['name' => 'shop_prize', 'display_name' => '奖品', 'children' => [
-                ['name' => 'shop_prize.coupon_batch', 'display_name' => '奖品列表'],
-                ['name' => 'shop_prize.coupon_batch_policy', 'display_name' => '奖品模板'],
-                ['name' => 'shop_prize.coupon_batch_policy_item', 'display_name' => '奖品模板子条目'],
+                ['name' => 'shop_prize.batch', 'display_name' => '奖品列表'],
+                ['name' => 'shop_prize.policy', 'display_name' => '奖品模板'],
                 ['name' => 'shop_prize.coupon', 'display_name' => '奖品核销'],
             ]],
             ['name' => 'shop_launch', 'display_name' => '投放', 'children' => [
