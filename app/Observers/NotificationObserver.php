85fc2fe36d116d23425cb0e97d1fddf30dea59d8
sylvia <xinyu_yuan@163.com>
2019-03-26 18:06:38 +0800

    通知监听发送微信模版消息

--- app/Observers/NotificationObserver.php
+++ app/Observers/NotificationObserver.php
@@ -22,18 +22,36 @@ class NotificationObserver
                 /** @var User $user */
                 $user = $databaseNotification->notifiable;
                 if ($user->weixin_openid) {
-                    $app = Factory::officialAccount(config('wechat.official_account'));
+                    $app = Factory::officialAccount(config('wechat.official_account.default'));
+
+
                     $content = $databaseNotification->data;
                     if ($content && $content['reply_content']) {
-                        $message = new Text($content['reply_content']);
-                        $app->customer_service->message($message)->to($user->weixin_openid)->send();
+
+                        $app->template_message->send([
+                            'touser' => $user->weixin_openid,
+                            'template_id' => config('wechat.official_account_template_id.notification'),
+                            'url' => null,
+                            'data' => [
+                                'first' => $user->name,
+                                'keyword1' => $content['reply_content'],
+                                'keyword2' => '无',
+                                'keyword3' => '无',
+                                'keyword4' => '无',
+                                'keyword5' => '数据中台',
+                                'remark' =>'',
+                            ]
+                        ]);
                     }
                 }
             }
         } catch (\Exception $e) {
             if (env('APP_ENV') == 'production') {
-                ding()->with('other')->text('发送微信消息出错：'.$e->getMessage());
+                ding()->with('other')->text('发送微信消息出错：' . $e->getMessage());
             }
+//            else if ($databaseNotification->notifiable->weixin_openid == 'oP41x03B8-rG3VnWAMqvFFVQr2G0') {
+//                abort(500, $e->getMessage());
+//            }
         }
     }
 
