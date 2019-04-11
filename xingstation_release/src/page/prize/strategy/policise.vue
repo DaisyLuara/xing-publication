1c804d4950dc31863a1ff7ecfdb6b7ed85d85400
daisyluara <1481795291@qq.com>
2019-04-11 15:48:01 +0800

    btn

--- xingstation_release/src/page/prize/strategy/policise.vue
+++ xingstation_release/src/page/prize/strategy/policise.vue
@@ -8,22 +8,25 @@
       <span class="label">数量: {{ pagination.total }}</span>
       <!-- 新增子策略 -->
       <div>
-        <el-button 
-          size="small" 
-          type="success" 
-          @click="addPolicy">新增子策略</el-button>
+        <el-button
+          size="small"
+          type="success"
+          @click="addPolicy"
+        >新增子策略</el-button>
       </div>
     </div>
     <!-- 子条目列表 -->
-    <el-table 
-      :data="tableData" 
-      style="width: 100%">
+    <el-table
+      :data="tableData"
+      style="width: 100%"
+    >
       <el-table-column type="expand">
         <template slot-scope="scope">
-          <el-form 
-            label-position="left" 
-            inline 
-            class="demo-table-expand">
+          <el-form
+            label-position="left"
+            inline
+            class="demo-table-expand"
+          >
             <el-form-item label="ID:">
               <span>{{ scope.row.id }}</span>
             </el-form-item>
@@ -42,49 +45,56 @@
           </el-form>
         </template>
       </el-table-column>
-      <el-table-column 
-        :show-overflow-tooltip="true" 
-        prop="id" 
-        label="ID" 
-        min-width="100"/>
-      <el-table-column 
-        :show-overflow-tooltip="true" 
-        prop="name" 
-        label="优惠券名称" 
-        min-width="130">
+      <el-table-column
+        :show-overflow-tooltip="true"
+        prop="id"
+        label="ID"
+        min-width="100"
+      />
+      <el-table-column
+        :show-overflow-tooltip="true"
+        prop="name"
+        label="优惠券名称"
+        min-width="130"
+      >
         <template slot-scope="scope">{{ scope.row.name }}</template>
       </el-table-column>
-      <el-table-column 
-        :show-overflow-tooltip="true" 
-        prop="company" 
-        label="公司名称" 
-        min-width="130">
+      <el-table-column
+        :show-overflow-tooltip="true"
+        prop="company"
+        label="公司名称"
+        min-width="130"
+      >
         <template slot-scope="scope">{{ scope.row.company.name }}</template>
       </el-table-column>
-      <el-table-column 
-        :show-overflow-tooltip="true" 
-        prop="rate" 
-        label="概率" 
-        min-width="130">
+      <el-table-column
+        :show-overflow-tooltip="true"
+        prop="rate"
+        label="概率"
+        min-width="100"
+      >
         <template slot-scope="scope">{{ scope.row.pivot.rate }} %</template>
       </el-table-column>
       <el-table-column
         :show-overflow-tooltip="true"
         prop="updated_at"
         label="更新时间"
-        min-width="100"
+        min-width="120"
       />
-      <el-table-column 
-        label="操作" 
-        min-width="100">
+      <el-table-column
+        label="操作"
+        min-width="120"
+      >
         <template slot-scope="scope">
-          <el-button 
-            size="small" 
-            type="warning" 
-            @click="editPolicy(scope.row)">编辑</el-button>
-          <el-button 
-            size="small" 
-            @click="deleteBatch(scope.row)">删除</el-button>
+          <el-button
+            size="small"
+            type="warning"
+            @click="editPolicy(scope.row)"
+          >编辑</el-button>
+          <el-button
+            size="small"
+            @click="deleteBatch(scope.row)"
+          >删除</el-button>
         </template>
       </el-table-column>
     </el-table>
