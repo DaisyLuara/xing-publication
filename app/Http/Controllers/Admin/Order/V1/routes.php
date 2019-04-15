a9fc60e8108001c359453e85f5f08f8e03679c02
caesar chan <caeasar_chan@live.com>
2019-04-15 12:10:38 +0800

    管理后台-订单发货

--- app/Http/Controllers/Admin/Order/V1/routes.php
+++ app/Http/Controllers/Admin/Order/V1/routes.php
@@ -11,7 +11,8 @@ $api->version('v1', [
 
         $api->group(['middleware' => "api.auth", 'model' => 'App\Models\User'], function ($api) {
 
-            $api->get('orders/{id}', ['as' => 'orders.show', 'uses' => 'OrderController@show']);
+            $api->get('orders/{order}', ['as' => 'orders.show', 'uses' => 'OrderController@show']);
+            $api->post('orders/{order}/ship', ['as' => 'orders.ship', 'uses' => 'OrderController@ship']);
             $api->get('orders', 'OrderController@index');
         });
 
