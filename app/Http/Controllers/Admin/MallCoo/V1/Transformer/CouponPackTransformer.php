71d30405b6a7eeb3a89152992c1812dd0ea7a813
fanzhuo <henbuhai@hotmail.com>
2019-04-23 11:30:55 +0800

    fix

--- app/Http/Controllers/Admin/MallCoo/V1/Transformer/CouponPackTransformer.php
+++ app/Http/Controllers/Admin/MallCoo/V1/Transformer/CouponPackTransformer.php
@@ -10,11 +10,8 @@ class CouponPackTransformer extends TransformerAbstract
     public function transform(CouponBatch $couponBatch)
     {
         return [
-            'id' => $couponBatch->id,
             'name' => $couponBatch->name,
             'description' => $couponBatch->description,
-            'image_url' => preg_replace('/exe666.com/','xingstation.cn', $couponBatch->image_url),
-            'bs_image_url' => preg_replace('/exe666.com/','xingstation.cn', $couponBatch->bs_image_url),
         ];
     }
 }
