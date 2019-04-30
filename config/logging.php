543ec4b1d02dcbddb4429aef023ce9596a022da2
yangyn <920176893@qq.com>
2019-04-30 09:36:53 +0800

    update

--- config/logging.php
+++ config/logging.php
@@ -54,7 +54,7 @@ return [
             'driver' => 'daily',
             'path' => $path,
             'level' => 'debug',
-            'days' => 1,
+            'days' => 7,
         ],
 
         'slack' => [
