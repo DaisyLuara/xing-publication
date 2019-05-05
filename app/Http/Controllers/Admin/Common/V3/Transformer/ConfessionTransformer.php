6677a5793bdff63713352af855ddd920c28186fd
fanzhuo <henbuhai@hotmail.com>
2019-05-05 10:31:05 +0800

    h5 告白页接口

---
+++ app/Http/Controllers/Admin/Common/V3/Transformer/ConfessionTransformer.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Common\V3\Transformer;
+
+use App\Http\Controllers\Admin\Common\V3\Models\Confession;
+use League\Fractal\TransformerAbstract;
+
+class ConfessionTransformer extends TransformerAbstract
+{
+
+    public function transform(Confession $confession)
+    {
+        return [
+            'name' => $confession->name,
+            'phone' => $confession->phone,
+            'message' => $confession->message,
+            'url' => $confession->media->url,
+            'created_at' => $confession->created_at->toDateTimeString(),
+            'updated_at' => $confession->updated_at->toDateTimeString(),
+        ];
+    }
+}
