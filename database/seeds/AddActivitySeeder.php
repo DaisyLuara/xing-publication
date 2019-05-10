7c2a4d05cd5d68c0c82373d45236d079d9b0cb3b
yangyn <920176893@qq.com>
2019-05-10 15:18:52 +0800

    update

--- database/seeds/AddActivitySeeder.php
+++ database/seeds/AddActivitySeeder.php
@@ -12,6 +12,6 @@ class AddActivitySeeder extends Seeder
      */
     public function run(): void
     {
-        Activity::create(['name' => '520全城告白']);
+        Activity::create(['utm_campaign' => 'Love520Action', 'name' => '520全城告白']);
     }
 }
