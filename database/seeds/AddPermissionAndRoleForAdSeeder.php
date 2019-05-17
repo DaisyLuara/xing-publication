dcf2b2b1cf33c2f762bd499e26cb7bb745167598
sylvia <xinyu_yuan@163.com>
2019-05-17 15:01:20 +0800

    广告权限

--- database/seeds/AddPermissionAndRoleForAdSeeder.php
+++ database/seeds/AddPermissionAndRoleForAdSeeder.php
@@ -15,14 +15,23 @@ class AddPermissionAndRoleForAdSeeder extends Seeder
     {
         app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
         //广告
-        $ad = Permission::query()->where('name','=','ad')->first()
+        $ad = Permission::query()->where('name', '=', 'ad')->first()
             ?? Permission::create(['name' => 'ad', 'display_name' => '广告']);
         //广告投放
-        $item =Permission::query()->where('name','=','ad.item')->first()
+        $item = Permission::query()->where('name', '=', 'ad.item')->first()
             ?? Permission::create(['name' => 'ad.item', 'display_name' => '广告投放', 'parent_id' => $ad->id]);
         //广告方案
-        $plan = Permission::query()->where('name','=','ad.plan')->first()
+        $plan = Permission::query()->where('name', '=', 'ad.plan')->first()
             ?? Permission::create(['name' => 'ad.plan', 'display_name' => '广告方案', 'parent_id' => $ad->id]);
+
+        //广告素材
+        $advertisement = Permission::query()->where('name', '=', 'ad.advertisement')->first()
+            ?? Permission::create(['name' => 'ad.advertisement', 'display_name' => '广告素材', 'parent_id' => $ad->id]);
+
+        //广告行业compo
+        $trade = Permission::query()->where('name', '=', 'ad.trade')->first()
+            ?? Permission::create(['name' => 'ad.trade', 'display_name' => '广告行业', 'parent_id' => $ad->id]);
+
 
         $data = [
             ['name' => 'ad.item.read', 'display_name' => '查看', 'parent_id' => $item->id],
@@ -32,10 +41,18 @@ class AddPermissionAndRoleForAdSeeder extends Seeder
             ['name' => 'ad.plan.read', 'display_name' => '查看', 'parent_id' => $plan->id],
             ['name' => 'ad.plan.create', 'display_name' => '新增', 'parent_id' => $plan->id],
             ['name' => 'ad.plan.update', 'display_name' => '修改', 'parent_id' => $plan->id],
+
+            ['name' => 'ad.advertisement.read', 'display_name' => '查看', 'parent_id' => $advertisement->id],
+            ['name' => 'ad.advertisement.create', 'display_name' => '新增', 'parent_id' => $advertisement->id],
+            ['name' => 'ad.advertisement.update', 'display_name' => '修改', 'parent_id' => $advertisement->id],
+
+            ['name' => 'ad.trade.read', 'display_name' => '查看', 'parent_id' => $trade->id],
+            ['name' => 'ad.trade.create', 'display_name' => '新增', 'parent_id' => $trade->id],
+            ['name' => 'ad.trade.update', 'display_name' => '修改', 'parent_id' => $trade->id],
         ];
 
         foreach ($data as $item) {
-            if (!Permission::query()->where('name','=',$item['name'])->first()) {
+            if (!Permission::query()->where('name', '=', $item['name'])->first()) {
                 Permission::create($item);
             }
         }
@@ -48,8 +65,13 @@ class AddPermissionAndRoleForAdSeeder extends Seeder
             'ad',
             'ad.item',
             'ad.plan',
+            'ad.advertisement',
+            'ad.trade',
+
             'ad.item.read',
             'ad.plan.read',
+            'ad.advertisement.read',
+            'ad.trade.read',
         ];
 
 
@@ -60,11 +82,17 @@ class AddPermissionAndRoleForAdSeeder extends Seeder
 
             'ad.plan.create',
             'ad.plan.update',
+
+            'ad.advertisement.create',
+            'ad.advertisement.update',
+
+            'ad.trade.create',
+            'ad.trade.update',
         ];
 
 
         /** @var Role $admin */
-        $admin = Role::query()->where('name','=','admin')->first();
+        $admin = Role::query()->where('name', '=', 'admin')->first();
 
         $admin->givePermissionTo(array_merge($readPermissionData, $editPermissionData));
     }
