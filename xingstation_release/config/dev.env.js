4f9b537adcc2563f2ab0f6ea965f74fe5713bd75
sylvia <xinyu_yuan@163.com>
2019-05-20 13:56:37 +0800

    remove update in dev.env.js

--- xingstation_release/config/dev.env.js
+++ xingstation_release/config/dev.env.js
@@ -3,7 +3,7 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  DOMAIN: '"jingfree.top"',
+  DOMAIN: '"xingstation.test"',
   SERVER_URL: '"http://api.ad.xingstation.test"',
   HTTPS_SERVER_URL: '"http://api.ad.xingstation.test"',
   CDN_URL: "'http://cdn.xingstation.cn/'"
