9f4db70040478d5fc80e808cbc7b1d424258b227
sylvia <xinyu_yuan@163.com>
2019-05-22 14:37:09 +0800

    fix

--- app/Http/Controllers/Admin/Ad/V1/Transformer/AdvertisementTransformer.php
+++ app/Http/Controllers/Admin/Ad/V1/Transformer/AdvertisementTransformer.php
@@ -13,7 +13,7 @@ class AdvertisementTransformer extends TransformerAbstract
             'id' => $advertisement->aid,
             'atid' =>  $advertisement->atid,
             'ad_trade_name' => $advertisement->ad_trade ? $advertisement->ad_trade->name : '',
-            'create_user_name' => ($advertisement->create_customer ? $advertisement->create_customer->name : '')
+            'create_user_name' => ($advertisement->create_customer ? $advertisement->create_customer->name : null)
                 ?? ($advertisement->create_user ? $advertisement->create_user->name : ''),
             'name' => $advertisement->name,
             'type' => $advertisement->type,
