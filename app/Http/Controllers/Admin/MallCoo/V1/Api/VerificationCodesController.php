8d64aab5ace8cc4af48e418002e06288e8bbd86d
fanzhuo <henbuhai@hotmail.com>
2019-06-05 18:44:21 +0800

    fix

--- app/Http/Controllers/Admin/MallCoo/V1/Api/VerificationCodesController.php
+++ app/Http/Controllers/Admin/MallCoo/V1/Api/VerificationCodesController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Admin\MallCoo\V1\Request\VerificationCodeRequest;
 use App\Http\Controllers\Controller;
 use App\Traits\UmsSms;
 use Log;
+use Overtrue\EasySms\EasySms;
 
 class VerificationCodesController extends Controller
 {
@@ -44,6 +45,40 @@ class VerificationCodesController extends Controller
         $key = 'verificationCode_' . str_random(15);
         $expiredAt = now()->addMinutes(30);
         // 缓存短信验证码 30分钟过期。
+        \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+
+        return $this->response->array([
+            'key' => $key,
+            'expired_at' => $expiredAt->toDateTimeString(),
+        ])->setStatusCode(201);
+    }
+
+    public function sendVerificationCodes(VerificationCodeRequest $request, EasySms $easySms)
+    {
+        $phone = $request->get('phone');
+
+        if (!app()->environment('production')) {
+            $code = '1234';
+        } else {
+            // 生成4位随机数，左侧补0
+            $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
+
+            $content = "【星视度】尊敬的用户您好，您的短信码：$code ， 有效期为10分钟，感谢您的参与！为保障您的账户安全，请勿泄露。";
+
+            try {
+                $easySms->send($phone, [
+                    'content' => $content,
+                ]);
+            } catch (\GuzzleHttp\Exception\ClientException $exception) {
+                $response = $exception->getResponse();
+                $result = json_decode($response->getBody()->getContents(), true);
+                return $this->response->errorInternal($result['msg'] ?? '短信发送异常');
+            }
+        }
+
+        $key = 'verificationCode_' . str_random(15);
+        $expiredAt = now()->addMinutes(10);
+        // 缓存短信验证码 10分钟过期。
         \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
 
         return $this->response->array([
